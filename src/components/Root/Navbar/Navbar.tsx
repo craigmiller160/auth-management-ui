2c4eec5c89f9cd3ea68ad2b222697a46eae980ac
Craig Miller <craigmiller160@gmail.com>
2020-07-16 17:58:51 -0400

    Added the AppBar

--- src/components/Root/Navbar/Navbar.tsx
+++ src/components/Root/Navbar/Navbar.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { NavLink } from 'react-router-dom';
+import './Navbar.scss';
 
-const Navbar = () => (
-    <h1>Navbar</h1>
-);
+const Navbar = () => {
+    return (
+        <AppBar position="static" className="Navbar">
+            <Toolbar>
+                <Button variant="text" color="inherit">
+                    <NavLink to="/" exact className="NavLink">
+                        <Typography variant="h6" noWrap>OAuth Management</Typography>
+                    </NavLink>
+                </Button>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
 export default Navbar;
