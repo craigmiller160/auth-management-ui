e41f08e1e9c9424f3032097669be6d64bc5e84f1
Craig Miller <craigmiller160@gmail.com>
2020-07-16 18:46:43 -0400

    Tried adding auth user loading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
+import React, { useEffect } from 'react';
+import { Provider as ReduxProvider, useDispatch } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from '../store';
 import Root from './Root';
 import theme from './theme';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { loadAuthUser } from '../store/auth/actions';
 
-const App = () => (
-    <ReduxProvider store={ store }>
-        <BrowserRouter>
-            <ThemeProvider theme={ theme }>
-                <Root />
-            </ThemeProvider>
-        </BrowserRouter>
-    </ReduxProvider>
-);
+const App = () => {
+    const dispatch = useDispatch(); // TODO move inside root
+    useEffect(() => {
+        dispatch(loadAuthUser());
+    }, [dispatch]);
+
+    return (
+        <ReduxProvider store={ store }>
+            <BrowserRouter>
+                <ThemeProvider theme={ theme }>
+                    <Root />
+                </ThemeProvider>
+            </BrowserRouter>
+        </ReduxProvider>
+    );
+}
 
 export default App;
