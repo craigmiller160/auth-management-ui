903caf680488755ed5b6dbbf9627d1dad12a6530
Craig Miller <craigmiller160@gmail.com>
2020-07-16 15:54:12 -0400

    Built full API service

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 import store from '../store';
 import alertSlice from '../store/alert/slice';
 import MessageBuilder from '../utils/MessageBuilder';
@@ -11,6 +11,15 @@ type SuppressErrorFn = (ex: Error) => Boolean
 interface ErrorResponse {
     status: number;
     message: string;
+}
+interface RequestConfig {
+    uri: string;
+    config: AxiosRequestConfig;
+    errorMsg?: string;
+    suppressError?: SuppressErrorFn
+}
+interface RequestBodyConfig extends RequestConfig {
+    body: any;
 }
 
 const isAxiosError = (ex: any): ex is AxiosError<ErrorResponse> => {
@@ -36,4 +45,51 @@ const handleError = (ex: Error, errorMsg: string = '', suppressError: SuppressEr
         console.log(fullMessage, ex);
         store.dispatch(alertSlice.actions.showErrorAlert(fullMessage));
     }
+};
+
+const get = async (req: RequestConfig): Promise<any> => {
+    try {
+        const res = await instance.get(req.uri, req.config);
+        return res.data;
+    } catch (ex) {
+        handleError(ex, req.errorMsg, req.suppressError);
+        return undefined;
+    }
+};
+
+const post = async (req: RequestBodyConfig): Promise<any> => {
+    try {
+        const res = await instance.post(req.uri, req.body, req.config);
+        return res.data;
+    } catch (ex) {
+        handleError(ex, req.errorMsg, req.suppressError);
+        return undefined;
+    }
+};
+
+const put = async (req: RequestBodyConfig): Promise<any> => {
+    try {
+        const res = await instance.put(req.uri, req.body, req.config);
+        return res.data;
+    } catch (ex) {
+        handleError(ex, req.errorMsg, req.suppressError);
+        return undefined;
+    }
+};
+
+const doDelete = async (req: RequestConfig): Promise<any> => {
+    try {
+        const res = await instance.delete(req.uri, req.config);
+        return res.data;
+    } catch (ex) {
+        handleError(ex, req.errorMsg, req.suppressError);
+        return undefined;
+    }
+};
+
+export default {
+    get,
+    put,
+    post,
+    delete: doDelete
 };
