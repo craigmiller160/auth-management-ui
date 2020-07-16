a1b02de24e0b15545da486e0317d3f0e00848f18
Craig Miller <craigmiller160@gmail.com>
2020-07-16 17:52:25 -0400

    Began stubbing out components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from '../store';
+import Root from './Root';
 
 const App = () => (
     <ReduxProvider store={ store }>
         <BrowserRouter>
-            <h1>Hello World</h1>
+            <Root />
         </BrowserRouter>
     </ReduxProvider>
 );
---
+++ src/components/Root/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar/Navbar';
+import Content from './Content';
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar />
+            <Content />
+        </div>
+    );
+};
+
+export default Root;
