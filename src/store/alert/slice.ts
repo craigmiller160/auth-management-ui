98d718a06ca782ce78a0190bfc83f97d17b06895
Craig Miller <craigmiller160@gmail.com>
2020-07-16 15:31:48 -0400

    Improved alert slice and began working on API services

--- src/store/alert/slice.ts
+++ src/store/alert/slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 const initialState = {
     show: false,
@@ -6,10 +6,30 @@ const initialState = {
     type: ''
 };
 
+type StateType = typeof initialState;
+
+const showErrorAlert = (state: StateType, action: PayloadAction<string>) => {
+    state.show = true;
+    state.type = 'error';
+    state.message = action.payload;
+};
+
+const showSuccessAlert = (state: StateType, action: PayloadAction<string>) => {
+    state.show = true;
+    state.type = 'success';
+    state.message = action.payload;
+};
+
+const hideAlert = (state: StateType) => {
+    state.show = false;
+};
+
 export default createSlice({
     name: 'alert',
     initialState,
     reducers: {
-
+        showErrorAlert,
+        showSuccessAlert,
+        hideAlert
     }
 });
