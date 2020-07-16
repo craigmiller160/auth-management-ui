2fc669b3c52ce791b3b2500812f98e6102239640
Craig Miller <craigmiller160@gmail.com>
2020-07-16 16:45:21 -0400

    Got option working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { getClients } from '../services/ClientService';
 import { ClientList } from '../types/api';
+import { Option } from 'fp-ts/es6/Option';
 
 const App = () => {
     useEffect(() => {
         getClients()
-            .then((res: ClientList | undefined) => {
+            .then((res: Option<ClientList>) => {
                 console.log(res); // TODO delete this
             });
     }, []);
