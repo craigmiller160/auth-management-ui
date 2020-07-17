4f42735f311feb57b13c2eea0b84e3423c468ec0
Craig Miller <craigmiller160@gmail.com>
2020-07-16 20:38:13 -0400

    Added Clients page

--- src/components/Root/Content/index.tsx
+++ src/components/Root/Content/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import PageHeader from '../../ui/PageHeader/PageHeader';
 import Container from '@material-ui/core/Container';
+import { Switch } from 'react-router';
+import ProtectedRoute from '../../routing/ProtectedRoute';
+import Clients from './Clients';
+
+const props = {
+    abc: 'def',
+    ghi: 'jkl'
+};
 
 const Content = () => (
     <Container>
         <PageHeader title="Welcome to OAuth Management" />
+        <Switch>
+            <ProtectedRoute
+                path="/clients"
+                componentProps={ props }
+                component={ Clients }
+            />
+        </Switch>
     </Container>
 );
 
--- src/components/routing/ProtectedRoute.tsx
+++ src/components/routing/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { ElementType, ComponentType } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 
-interface Rule {
+export interface Rule {
     allow: () => boolean;
     redirect: string;
 }
@@ -10,7 +10,7 @@ interface Props {
     rules?: Array<Rule>;
     path: string;
     componentProps: object; // TODO make this generic
-    component: any; // TODO need to type this better
+    component: ElementType | ComponentType;
     exact?: boolean;
     routeKey?: string;
 }
@@ -18,7 +18,7 @@ interface Props {
 const ProtectedRoute = (props: Props) => {
     const failedRule = props.rules?.find((rule: Rule) => !rule.allow());
     if (failedRule) {
-        return <Redirect to={ failedRule.redirect } />
+        return <Redirect to={ failedRule.redirect } />;
     }
 
     const Component = props.component;
