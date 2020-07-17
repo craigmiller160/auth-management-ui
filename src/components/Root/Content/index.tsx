ca6a2f8e5e2ad67c8f8a917a8af096149fb4c876
Craig Miller <craigmiller160@gmail.com>
2020-07-16 20:41:55 -0400

    Added first rule for protected routes

--- src/components/Root/Content/index.tsx
+++ src/components/Root/Content/index.tsx
@@ -2,19 +2,33 @@ import React from 'react';
 import PageHeader from '../../ui/PageHeader/PageHeader';
 import Container from '@material-ui/core/Container';
 import { Switch } from 'react-router';
-import ProtectedRoute from '../../routing/ProtectedRoute';
+import ProtectedRoute, { Rule } from '../../routing/ProtectedRoute';
 import Clients from './Clients';
+import { useSelector } from 'react-redux';
+import { isAuthorized } from '../../../store/auth/selectors';
 
-const Content = () => (
-    <Container>
-        <PageHeader title="Welcome to OAuth Management" />
-        <Switch>
-            <ProtectedRoute
-                path="/clients"
-                component={ Clients }
-            />
-        </Switch>
-    </Container>
-);
+const Content = () => {
+    const isAuth = useSelector(isAuthorized);
+
+    const isAuthRule = {
+        allow: () => isAuth,
+        redirect: '/'
+    };
+
+    return (
+        <Container>
+            <PageHeader title="Welcome to OAuth Management" />
+            <Switch>
+                <ProtectedRoute
+                    path="/clients"
+                    component={ Clients }
+                    rules={ [
+                        isAuthRule
+                    ] }
+                />
+            </Switch>
+        </Container>
+    );
+};
 
 export default Content;
