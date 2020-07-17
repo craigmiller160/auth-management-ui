3ef1930bd6886d794907ccea596094b243d74ae0
Craig Miller <craigmiller160@gmail.com>
2020-07-16 20:48:33 -0400

    Fixed Root component

--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -1,19 +1,26 @@
 import React, { useEffect } from 'react';
 import Navbar from './Navbar/Navbar';
 import Content from './Content';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { loadAuthUser } from '../../store/auth/actions';
+import { RootState } from '../../store';
 
 const Root = () => {
     const dispatch = useDispatch();
+    const hasChecked = useSelector((state: RootState) => state.auth.hasChecked);
     useEffect(() => {
         dispatch(loadAuthUser());
     }, [dispatch]);
 
     return (
         <div>
-            <Navbar />
-            <Content />
+            {
+                hasChecked &&
+                <>
+                    <Navbar />
+                    <Content />
+                </>
+            }
         </div>
     );
 };
