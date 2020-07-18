d91e999d84140bde15e10a988baf84ad530c5418
Craig Miller <craigmiller160@gmail.com>
2020-07-18 11:54:29 -0400

    Improved type validations

--- src/components/Root/Content/Clients/ClientDetails/index.tsx
+++ src/components/Root/Content/Clients/ClientDetails/index.tsx
@@ -32,6 +32,24 @@ interface MatchParams {
 }
 
 interface ClientForm extends Omit<Client, 'id'> { }
+const NEW = 'new';
+const NAME = 'name';
+const CLIENT_KEY = 'clientKey';
+const CLIENT_SECRET = 'clientSecret';
+const ACCESS_TOKEN_TIMEOUT = 'accessTokenTimeoutSecs';
+const REFRESH_TOKEN_TIMEOUT = 'refreshTokenTimeoutSecs';
+const ENABLED = 'enabled';
+const ALLOW_CLIENT_CREDS = 'allowClientCredentials';
+const ALLOW_PASSWORD = 'allowPassword';
+const ALLOW_AUTH_CODE = 'allowAuthCode';
+
+const STRING_PROPS = [NAME, CLIENT_KEY, CLIENT_SECRET];
+const NUMBER_PROPS = [ACCESS_TOKEN_TIMEOUT, REFRESH_TOKEN_TIMEOUT];
+const BOOLEAN_PROPS = [ENABLED, ALLOW_CLIENT_CREDS, ALLOW_PASSWORD, ALLOW_AUTH_CODE];
+
+type ClientStringProperty = typeof NAME | typeof CLIENT_KEY | typeof CLIENT_SECRET;
+type ClientNumberProperty = typeof ACCESS_TOKEN_TIMEOUT | typeof REFRESH_TOKEN_TIMEOUT;
+type ClientBooleanProperty = typeof ENABLED | typeof ALLOW_CLIENT_CREDS | typeof ALLOW_PASSWORD | typeof ALLOW_AUTH_CODE;
 
 const defaultClient: Client = {
     id: 0,
@@ -46,21 +64,16 @@ const defaultClient: Client = {
     accessTokenTimeoutSecs: 0
 };
 
-const NEW = 'new';
-
-// TODO improve the use of literal strings here
-
-const isStringProperty = (name: string): name is 'name' | 'clientKey' | 'clientSecret' => {
-    return name === 'name' || name === 'clientKey' || name === 'clientSecret';
+const isStringProperty = (name: string): name is ClientStringProperty => {
+    return STRING_PROPS.includes(name);
 };
 
-const isNumberProperty = (name: string): name is 'accessTokenTimeoutSecs' | 'refreshTokenTimeoutSecs' => {
-    return name === 'accessTokenTimeoutSecs' || name === 'refreshTokenTimeoutSecs';
+const isNumberProperty = (name: string): name is ClientNumberProperty => {
+    return NUMBER_PROPS.includes(name);
 };
 
-const isBooleanProperty = (name: string): name is 'enabled' | 'allowClientCredentials' | 'allowPassword' | 'allowAuthCode' => {
-    return name === 'enabled' || name === 'allowClientCredentials'
-        || name === 'allowPassword' || name === 'allowAuthCode';
+const isBooleanProperty = (name: string): name is ClientBooleanProperty => {
+    return BOOLEAN_PROPS.includes(name);
 };
 
 const ClientDetails = () => {
