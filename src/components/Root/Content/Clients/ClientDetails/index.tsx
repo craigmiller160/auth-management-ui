ca8efab267a80a707761532e220dac45ce4f545c
Craig Miller <craigmiller160@gmail.com>
2020-07-18 10:42:21 -0400

    Began writing save logic

--- src/components/Root/Content/Clients/ClientDetails/index.tsx
+++ src/components/Root/Content/Clients/ClientDetails/index.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, useEffect } from 'react';
-import { useHistory, useRouteMatch } from 'react-router';
-import { isSome } from 'fp-ts/es6/Option';
-import { generateGuid, getClient } from '../../../../../services/ClientService';
+import { Prompt, useHistory, useRouteMatch } from 'react-router';
+import { isSome, Option } from 'fp-ts/es6/Option';
+import { createClient, generateGuid, getClient, updateClient } from '../../../../../services/ClientService';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import { Client } from '../../../../../types/api';
@@ -17,7 +17,8 @@ import { greaterThanZero } from '../../../../../utils/validations';
 
 interface State {
     client: Partial<Client>;
-    showCancelDialog: boolean;
+    shouldBlockNavigation: boolean;
+    showDeleteDialog: boolean;
 }
 
 interface MatchParams {
@@ -34,13 +35,31 @@ const ClientDetails = () => {
     const id = match.params.id;
     const [state, setState] = useImmer<State>({
         client: {},
-        showCancelDialog: false
+        shouldBlockNavigation: true,
+        showDeleteDialog: false
     });
     const { register, handleSubmit, errors } = useForm<ClientForm>({
         mode: 'onBlur',
         reValidateMode: 'onChange'
     });
-    const onSubmit = (data: ClientForm) => console.log('Submit', data);
+
+    const doSubmit = async (action: () => Promise<Option<any>>) => {
+        const result = await action();
+        if (isSome(result)) {
+            setState((draft) => {
+                draft.shouldBlockNavigation = false;
+            });
+            history.push('/clients');
+        }
+    };
+
+    const onSubmit = () => {
+        if (id === 'new') {
+            doSubmit(() => createClient(state.client));
+        } else {
+            doSubmit(() => updateClient(parseInt(id), state.cient));
+        }
+    };
 
     useEffect(() => {
         const action = async () => {
@@ -74,16 +93,6 @@ const ClientDetails = () => {
 
         action();
     }, [id, setState]);
-
-    const doCancel = () => {
-        setState((draft) => {
-            draft.showCancelDialog = false;
-        });
-        history.push('/clients');
-    };
-    const toggleCancelDialog = (show: boolean) => setState((draft) => {
-        draft.showCancelDialog = show;
-    });
 
     const inputChange = (event: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
@@ -148,208 +157,214 @@ const ClientDetails = () => {
         }
     };
 
+    const doCancel = () => history.push('/clients');
+
     return (
-        <div className="ClientDetails">
-            <PageHeader title="Client Details" />
-            <form onSubmit={ handleSubmit(onSubmit) }>
-                <Grid
-                    container
-                    direction="row"
-                    justify="center"
-                >
-                    <TextField
-                        className="grow-sm"
-                        label="Client Name"
-                        name="name"
-                        value={ state.client.name ?? '' }
-                        onChange={ inputChange }
-                        inputRef={ register({ required: 'Required' }) }
-                        error={ !!errors.name }
-                        helperText={ errors.name?.message ?? '' }
-                    />
-                </Grid>
-                <SectionHeader title="Keys" />
-                <Grid
-                    container
-                    direction="row"
-                >
+        <>
+            <Prompt
+                when={ state.shouldBlockNavigation }
+                message="Are you sure you want to leave? Any unsaved changes will be lost."
+            />
+            <div className="ClientDetails">
+                <PageHeader title="Client Details" />
+                <form onSubmit={ handleSubmit(onSubmit) }>
                     <Grid
                         container
                         direction="row"
                         justify="center"
-                        className="half-row"
                     >
                         <TextField
-                            className="grow"
-                            label="Client Key"
-                            name="clientKey"
-                            value={ state.client.clientKey ?? '' }
+                            className="grow-sm"
+                            label="Client Name"
+                            name="name"
+                            value={ state.client.name ?? '' }
                             onChange={ inputChange }
                             inputRef={ register({ required: 'Required' }) }
-                            error={ !!errors.clientKey }
-                            helperText={ errors.clientKey?.message ?? '' }
-                            disabled
+                            error={ !!errors.name }
+                            helperText={ errors.name?.message ?? '' }
                         />
-                        <Button
-                            variant="text"
-                            color="default"
-                            onClick={ generateClientKey }
-                        >
-                            Generate
-                        </Button>
                     </Grid>
+                    <SectionHeader title="Keys" />
                     <Grid
                         container
                         direction="row"
-                        justify="center"
-                        className="half-row"
+                    >
+                        <Grid
+                            container
+                            direction="row"
+                            justify="center"
+                            className="half-row"
+                        >
+                            <TextField
+                                className="grow"
+                                label="Client Key"
+                                name="clientKey"
+                                value={ state.client.clientKey ?? '' }
+                                onChange={ inputChange }
+                                inputRef={ register({ required: 'Required' }) }
+                                error={ !!errors.clientKey }
+                                helperText={ errors.clientKey?.message ?? '' }
+                                disabled
+                            />
+                            <Button
+                                variant="text"
+                                color="default"
+                                onClick={ generateClientKey }
+                            >
+                                Generate
+                            </Button>
+                        </Grid>
+                        <Grid
+                            container
+                            direction="row"
+                            justify="center"
+                            className="half-row"
+                        >
+                            <TextField
+                                className="grow"
+                                label="Client Secret"
+                                name="clientSecret"
+                                value={ state.client.clientSecret ?? '' }
+                                onChange={ inputChange }
+                                inputRef={ register }
+                                error={ !!errors.clientSecret }
+                                helperText={ errors.clientSecret?.message ?? '' }
+                                disabled
+                            />
+                            <Button
+                                variant="text"
+                                color="default"
+                                onClick={ generateClientSecret }
+                            >
+                                Generate
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    <SectionHeader title="Grants" />
+                    <Grid
+                        container
+                        direction="row"
+                        justify="space-around"
+                    >
+                        <FormControlLabel
+                            label="Enabled"
+                            control={
+                                <Checkbox
+                                    name="enabled"
+                                    checked={ state.client.enabled ?? false }
+                                    onChange={ checkboxChange }
+                                    color="primary"
+                                />
+                            }
+                        />
+                        <FormControlLabel
+                            label="Client Credentials Grant"
+                            control={
+                                <Checkbox
+                                    name="allowClientCredentials"
+                                    checked={ state.client.allowClientCredentials ?? false }
+                                    onChange={ checkboxChange }
+                                    color="primary"
+                                />
+                            }
+                        />
+                        <FormControlLabel
+                            label="Password Grant"
+                            control={
+                                <Checkbox
+                                    name="allowPassword"
+                                    checked={ state.client.allowPassword ?? false }
+                                    onChange={ checkboxChange }
+                                    color="primary"
+                                />
+                            }
+                        />
+                        <FormControlLabel
+                            label="Authorization Code Grant"
+                            control={
+                                <Checkbox
+                                    name="allowAuthCode"
+                                    checked={ state.client.allowAuthCode ?? false }
+                                    onChange={ checkboxChange }
+                                    color="primary"
+                                />
+                            }
+                        />
+                    </Grid>
+                    <SectionHeader title="Timeouts" />
+                    <Grid
+                        container
+                        direction="row"
+                        justify="space-around"
                     >
                         <TextField
-                            className="grow"
-                            label="Client Secret"
-                            name="clientSecret"
-                            value={ state.client.clientSecret ?? '' }
+                            className="timeouts"
+                            type="number"
+                            label="Access Token Timeout (Secs)"
+                            name="accessTokenTimeoutSecs"
+                            value={ state.client.accessTokenTimeoutSecs ?? '' }
                             onChange={ inputChange }
-                            inputRef={ register }
-                            error={ !!errors.clientSecret }
-                            helperText={ errors.clientSecret?.message ?? '' }
-                            disabled
+                            inputRef={ register({
+                                required: 'Required',
+                                validate: {
+                                    greaterThanZero
+                                }
+                            }) }
+                            error={ !!errors.accessTokenTimeoutSecs }
+                            helperText={ errors.accessTokenTimeoutSecs?.message ?? '' }
                         />
+                        <TextField
+                            className="timeouts"
+                            type="number"
+                            label="Refresh Token Timeout (Secs)"
+                            name="refreshTokenTimeoutSecs"
+                            value={ state.client.refreshTokenTimeoutSecs ?? '' }
+                            onChange={ inputChange }
+                            inputRef={ register({
+                                required: 'Required',
+                                validate: {
+                                    greaterThanZero
+                                }
+                            }) }
+                            error={ !!errors.refreshTokenTimeoutSecs }
+                            helperText={ errors.refreshTokenTimeoutSecs?.message ?? '' }
+                        />
+                    </Grid>
+                    <SectionHeader title="Actions" />
+                    <Grid
+                        container
+                        direction="row"
+                        justify="space-around"
+                    >
                         <Button
-                            variant="text"
-                            color="default"
-                            onClick={ generateClientSecret }
+                            variant="contained"
+                            color="primary"
+                            onClick={ doCancel }
                         >
-                            Generate
+                            Cancel
+                        </Button>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                        >
+                            Save
+                        </Button>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                        >
+                            Delete
                         </Button>
                     </Grid>
-                </Grid>
-                <SectionHeader title="Grants" />
-                <Grid
-                    container
-                    direction="row"
-                    justify="space-around"
-                >
-                    <FormControlLabel
-                        label="Enabled"
-                        control={
-                            <Checkbox
-                                name="enabled"
-                                checked={ state.client.enabled ?? false }
-                                onChange={ checkboxChange }
-                                color="primary"
-                            />
-                        }
-                    />
-                    <FormControlLabel
-                        label="Client Credentials Grant"
-                        control={
-                            <Checkbox
-                                name="allowClientCredentials"
-                                checked={ state.client.allowClientCredentials ?? false }
-                                onChange={ checkboxChange }
-                                color="primary"
-                            />
-                        }
-                    />
-                    <FormControlLabel
-                        label="Password Grant"
-                        control={
-                            <Checkbox
-                                name="allowPassword"
-                                checked={ state.client.allowPassword ?? false }
-                                onChange={ checkboxChange }
-                                color="primary"
-                            />
-                        }
-                    />
-                    <FormControlLabel
-                        label="Authorization Code Grant"
-                        control={
-                            <Checkbox
-                                name="allowAuthCode"
-                                checked={ state.client.allowAuthCode ?? false }
-                                onChange={ checkboxChange }
-                                color="primary"
-                            />
-                        }
-                    />
-                </Grid>
-                <SectionHeader title="Timeouts" />
-                <Grid
-                    container
-                    direction="row"
-                    justify="space-around"
-                >
-                    <TextField
-                        className="timeouts"
-                        type="number"
-                        label="Access Token Timeout (Secs)"
-                        name="accessTokenTimeoutSecs"
-                        value={ state.client.accessTokenTimeoutSecs ?? '' }
-                        onChange={ inputChange }
-                        inputRef={ register({
-                            required: 'Required',
-                            validate: {
-                                greaterThanZero
-                            }
-                        }) }
-                        error={ !!errors.accessTokenTimeoutSecs }
-                        helperText={ errors.accessTokenTimeoutSecs?.message ?? '' }
-                    />
-                    <TextField
-                        className="timeouts"
-                        type="number"
-                        label="Refresh Token Timeout (Secs)"
-                        name="refreshTokenTimeoutSecs"
-                        value={ state.client.refreshTokenTimeoutSecs ?? '' }
-                        onChange={ inputChange }
-                        inputRef={ register({
-                            required: 'Required',
-                            validate: {
-                                greaterThanZero
-                            }
-                        }) }
-                        error={ !!errors.refreshTokenTimeoutSecs }
-                        helperText={ errors.refreshTokenTimeoutSecs?.message ?? '' }
-                    />
-                </Grid>
-                <SectionHeader title="Actions" />
-                <Grid
-                    container
-                    direction="row"
-                    justify="space-around"
-                >
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={ () => toggleCancelDialog(true) }
-                    >
-                        Cancel
-                    </Button>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        type="submit"
-                    >
-                        Save
-                    </Button>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                    >
-                        Delete
-                    </Button>
-                </Grid>
-            </form>
-            <ConfirmDialog
-                open={ state.showCancelDialog }
-                title="Cancel Changes"
-                message="Are you sure you want to cancel all changes?"
-                onConfirm={ doCancel }
-                onCancel={ () => toggleCancelDialog(false) }
-            />
-        </div>
+                </form>
+                <ConfirmDialog
+                    open={ state.showDeleteDialog }
+                    title="Delete Client"
+                    message="Are you sure you want to delete this client?"
+                />
+            </div>
+        </>
     );
 };
 
