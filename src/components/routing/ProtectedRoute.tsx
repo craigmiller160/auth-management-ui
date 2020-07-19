335768319eb786e7922710bf9cdb16bdcabcdfa5
Craig Miller <craigmiller160@gmail.com>
2020-07-19 08:35:39 -0400

    Got protected route working

--- src/components/routing/ProtectedRoute.tsx
+++ src/components/routing/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
-import React, { ElementType, ComponentType, useEffect } from 'react';
-import { Redirect, Route, useHistory } from 'react-router-dom';
+import React, { ComponentType, ElementType } from 'react';
+import { Redirect, Route } from 'react-router-dom';
 
 export interface Rule<R extends object> {
     allow: (ruleProps?: R) => boolean;
@@ -16,24 +16,11 @@ interface Props<T extends object,R extends object> {
     routeKey?: string;
 }
 
-// TODO missing piece is recording isAuth = false for all 401s
-
 const ProtectedRoute = <T extends object,R extends object>(props: Props<T,R>) => {
-    const history = useHistory();
-    useEffect(() => {
-        const unlisten = history.listen((location, action) => {
-            console.log(location, action); // TODO delete this
-            console.log(props.ruleProps); // TODO delete this
-            const failedRule = props.rules?.find((rule: Rule<R>) => !rule.allow(props.ruleProps));
-            if (failedRule) {
-                console.log('Failed rule'); // TODO delete this
-                // return <Redirect to={ failedRule.redirect } />;
-            }
-        });
-        return () => {
-            unlisten();
-        };
-    }, []);
+    const failedRule = props.rules?.find((rule: Rule<R>) => !rule.allow(props.ruleProps));
+    if (failedRule) {
+        return <Redirect to={ failedRule.redirect } />;
+    }
 
     const Component = props.component;
 
