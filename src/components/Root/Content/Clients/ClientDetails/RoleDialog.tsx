73af486c58dca7eca852de8d7f1cef603ca7a545
Craig Miller <craigmiller160@gmail.com>
2020-07-19 17:12:23 -0400

    Created role dialog

--- src/components/Root/Content/Clients/ClientDetails/RoleDialog.tsx
+++ src/components/Root/Content/Clients/ClientDetails/RoleDialog.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import BaseDialog from '../../../../ui/Dialog/BaseDialog';
+import React, { ChangeEvent } from 'react';
+import BaseDialog, { DialogAction } from '../../../../ui/Dialog/BaseDialog';
 import TextField from '@material-ui/core/TextField';
 import { useImmer } from 'use-immer';
 import { useForm } from 'react-hook-form';
@@ -26,14 +26,33 @@ const RoleDialog = (props: Props) => {
         reValidateMode: 'onChange'
     });
 
+    const onSubmit = (arg: any) => console.log(arg); // TODO delete this
+
+    const actions: Array<DialogAction> = [
+        { label: 'Save', onClick: handleSubmit(onSubmit) }
+    ];
+
+    const setName = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setState((draft) => {
+            draft.name = value;
+        });
+    };
+
     return (
         <BaseDialog
             open={ open }
             title={ title }
-            actions={ [] }
+            actions={ actions }
         >
             <TextField
-
+                name="name"
+                label="Role Name"
+                value={ state.name }
+                onChange={ setName }
+                inputRef={ register({ required: 'Required' }) }
+                error={ !!errors.name }
+                helperText={ errors.name?.message ?? '' }
             />
         </BaseDialog>
     );
