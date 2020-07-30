96613b4deb31d8c628d69d7e51a64d9a0e22300f
Craig Miller <craigmiller160@gmail.com>
2020-07-30 08:40:29 -0400

    Refactored away the old types

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,6 +1,5 @@
-import { Option } from 'fp-ts/es6/Option';
 import api, { isAxiosError } from './Api';
-import { AuthUser } from '../types/oldApi';
+import { AuthUser } from '../types/auth';
 import { Either } from 'fp-ts/es6/Either';
 
 export const logout = (): Promise<Either<Error, void>> =>
