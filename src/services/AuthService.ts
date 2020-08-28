7eea7c9a2a96343f3f52ba89a2acd877ef7d32ad
Craig Miller <craigmiller160@gmail.com>
2020-08-28 10:23:31 -0400

    Fixed how the login is handled to make it a post request with an Origin header

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,12 +1,25 @@
 import api, { isAxiosError } from './Api';
-import { AuthUser } from '../types/auth';
-import { Either } from 'fp-ts/es6/Either';
+import { AuthCodeLogin, AuthUser } from '../types/auth';
+import { Either, map } from 'fp-ts/es6/Either';
+import { pipe } from 'fp-ts/es6/pipeable';
 
 export const logout = (): Promise<Either<Error, void>> =>
     api.get<void>({
         uri: '/oauth/logout',
         errorMsg: 'Error logging out'
     });
+
+export const login = async (): Promise<Either<Error, AuthCodeLogin>> =>
+    pipe(
+        await api.post<void,AuthCodeLogin>({
+            uri: '/oauth/authcode/login',
+            errorMsg: 'Error getting login URL'
+        }),
+        map((login: AuthCodeLogin) => {
+            window.location.href = login.url;
+            return login;
+        })
+    );
 
 export const getAuthUser = (): Promise<Either<Error, AuthUser>> =>
     api.get<AuthUser>({
