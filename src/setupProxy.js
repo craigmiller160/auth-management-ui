a40974a530a898db50b5d1b2dfdd7951db214502
Craig Miller <craigmiller160@gmail.com>
2020-08-28 10:52:14 -0400

    Enhanced proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,9 +4,19 @@ const createProxy = (app) => {
     app.use(createProxyMiddleware('/api', {
         target: 'https://localhost:7004',
         changeOrigin: true,
-        secure: false,
+        secure: true,
         pathRewrite: {
             '^/api': ''
+        },
+        logLevel: 'debug'
+    }));
+
+    app.use(createProxyMiddleware('/oauth2', {
+        target: '',
+        changeOrigin: true,
+        secure: true,
+        pathRewrite: {
+            '^/oauth2': ''
         },
         logLevel: 'debug'
     }));
