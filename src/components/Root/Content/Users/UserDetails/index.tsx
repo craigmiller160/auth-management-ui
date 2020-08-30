ae6411a8cad02ea29cf21132a1757eeafcfd58ff
Craig Miller <craigmiller160@gmail.com>
2020-08-30 10:13:24 -0400

    Began laying out page

--- src/components/Root/Content/Users/UserDetails/index.tsx
+++ src/components/Root/Content/Users/UserDetails/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router';
+import { useImmer } from 'use-immer';
+import { PageHeader } from '../../../../ui/Header';
 
-const UserDetails = () => (
-    <div>
-        <h1>User Details</h1>
-    </div>
-);
+interface State {
+    userId: number;
+}
+
+interface MatchParams {
+    id: string;
+}
+const NEW = 'new';
+
+const UserDetails = () => {
+    const match = useRouteMatch<MatchParams>();
+    const id = match.params.id;
+    const [state, setState] = useImmer<State>({
+        userId: id !== NEW ? parseInt(id) : 0
+    });
+
+    return (
+        <div className="UserDetails">
+            <PageHeader title="User Details" />
+        </div>
+    );
+}
 
 export default UserDetails;
