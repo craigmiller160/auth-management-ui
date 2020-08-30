a40c46f8339648c126f99bdaa0e3c5886a7efdcb
Craig Miller <craigmiller160@gmail.com>
2020-08-30 17:54:00 -0400

    Copied over existing UserClients component to new page

---
+++ src/components/Root/Content/Users/UserDetails/UserClients.tsx
@@ -0,0 +1,173 @@
+import React, { useEffect, useMemo } from 'react';
+import { ClientListItem, ClientListResponse } from '../../../../../types/client';
+import { UserClient } from '../../../../../types/user';
+import { useHistory } from 'react-router';
+import { useImmer } from 'use-immer';
+import { SectionHeader } from '../../../../ui/Header';
+import List, { Item } from '../../../../ui/List';
+import Button from '@material-ui/core/Button';
+import { Business } from '@material-ui/icons';
+import { exists, Option } from 'fp-ts/es6/Option';
+import { ConfirmDialog, SelectDialog } from '../../../../ui/Dialog';
+import { SelectOption } from '../../../../ui/Form/Autocomplete';
+import { pipe } from 'fp-ts/es6/pipeable';
+import { addClientToUser, removeClientFromUser } from '../../../../../services/UserService';
+import { getOrElse, map } from 'fp-ts/es6/Either';
+import { getAllClients } from '../../../../../services/ClientService';
+
+interface Props {
+    userClients: Array<UserClient>;
+    userId: number;
+    updateClients: (clients: Array<UserClient>) => void;
+    selectedClient: Option<UserClient>;
+    selectClient: (client: UserClient) => void;
+}
+
+interface State {
+    allClients: Array<ClientListItem>;
+    showAddClientDialog: boolean;
+    showRemoveClientDialog: boolean;
+    clientIdToRemove: number;
+}
+
+const UserClients = (props: Props) => {
+    const {
+        userClients,
+        userId,
+        updateClients,
+        selectedClient,
+        selectClient
+    } = props;
+
+    const history = useHistory();
+    const [state, setState] = useImmer<State>({
+        allClients: [],
+        showAddClientDialog: false,
+        showRemoveClientDialog: false,
+        clientIdToRemove: 0
+    });
+
+    useEffect(() => {
+        const action = async () => {
+            const clients = pipe(
+                await getAllClients(),
+                map((res: ClientListResponse) => res.clients),
+                getOrElse((): Array<ClientListItem> => [])
+            );
+
+            setState((draft) => {
+                draft.allClients = clients;
+            });
+        };
+
+        action();
+    }, [setState]);
+
+    const goToClient = (clientId: number) =>
+        history.push(`/clients/${clientId}`);
+
+    const removeClientClick = (clientId: number) => {
+        setState((draft) => {
+            draft.clientIdToRemove = clientId;
+            draft.showRemoveClientDialog = true;
+        });
+    };
+
+    const clientItems: Array<Item> = userClients.map((client) => ({
+        click: () => selectClient(client),
+        avatar: () => <Business />,
+        text: {
+            primary: client.name
+        },
+        secondaryActions: [
+            {
+                text: 'Go',
+                click: () => goToClient(client.id)
+            },
+            {
+                text: 'Remove',
+                click: () => removeClientClick(client.id)
+            }
+        ],
+        active: exists((selected: UserClient) => selected.id === client.id)(selectedClient)
+    }));
+
+    const addClientClick = () =>
+        setState((draft) => {
+            draft.showAddClientDialog = true;
+        });
+
+    const addClientSelect = async (selectedClient: SelectOption<number>) => {
+        setState((draft) => {
+            draft.showAddClientDialog = false
+        });
+        const clientId = selectedClient.value;
+        const clients = pipe (
+            await addClientToUser(userId, clientId),
+            getOrElse((): Array<UserClient> => [])
+        );
+        updateClients(clients);
+    };
+
+    const addClientCancel = () =>
+        setState((draft) => {
+            draft.showAddClientDialog = false;
+        });
+
+    const clientOptions = useMemo(() =>
+            state.allClients
+                .filter((client) => !userClients.find((otherClient) => client.id === otherClient.id))
+                .sort((client1, client2) => client1.name.localeCompare(client2.name))
+                .map((client) => ({
+                    label: client.name,
+                    value: client.id
+                })),
+        [state.allClients, userClients]);
+
+    const removeClientOnCancel = () =>
+        setState((draft) => {
+            draft.showRemoveClientDialog = false;
+        });
+
+    const removeClientOnConfirm = async () => {
+        setState((draft) => {
+            draft.showRemoveClientDialog = false;
+        });
+        const clients = pipe(
+            await removeClientFromUser(userId, state.clientIdToRemove),
+            getOrElse((): Array<UserClient> => [])
+        );
+        updateClients(clients);
+    };
+
+    return (
+        <>
+            <SectionHeader title="Clients" />
+            <List items={ clientItems } />
+            <Button
+                color="primary"
+                variant="contained"
+                onClick={ addClientClick }
+            >
+                Add Client
+            </Button>
+            <SelectDialog
+                open={ state.showAddClientDialog }
+                title="Add Client"
+                onSelect={ addClientSelect }
+                onCancel={ addClientCancel }
+                options={ clientOptions }
+                label="Select Client"
+            />
+            <ConfirmDialog
+                open={ state.showRemoveClientDialog }
+                title="Remove Client"
+                message="Are you sure you want to remove this client from this user?"
+                onConfirm={ removeClientOnConfirm }
+                onCancel={ removeClientOnCancel }
+            />
+        </>
+    );
+};
+
+export default UserClients;
--- src/components/Root/Content/Users/UserDetails/UserGrants.tsx
+++ src/components/Root/Content/Users/UserDetails/UserGrants.tsx
@@ -4,7 +4,7 @@ import { useImmer } from 'use-immer';
 import { pipe } from 'fp-ts/es6/pipeable';
 import { getUserClients } from '../../../../../services/UserService';
 import { getOrElse, map } from 'fp-ts/es6/Either';
-import { UserClient, UserClients } from '../../../../../types/user';
+import { UserClient, UserClients as UserClientsType } from '../../../../../types/user';
 import { Grid, Typography } from '@material-ui/core';
 import './UserGrants.scss';
 import { SectionHeader } from '../../../../ui/Header';
@@ -14,11 +14,11 @@ import { SelectOption } from '../../../../ui/Form/Autocomplete';
 import { Item } from '../../../../ui/List';
 import { Business } from '@material-ui/icons';
 import { exists, none, Option, some } from 'fp-ts/es6/Option';
+import UserClients from './UserClients';
 
 interface State {
     userId: number;
-    user: UserClients;
-    allClients: Array<ClientListItem>;
+    user: UserClientsType;
     selectedClient: Option<UserClient>;
 }
 const NEW = 'new';
@@ -29,7 +29,7 @@ interface Props {
     match: match<MatchParams>;
 }
 
-const defaultUser: UserClients = {
+const defaultUser: UserClientsType = {
     id: 0,
     email: '',
     clients: []
@@ -40,77 +40,27 @@ const UserGrants = (props: Props) => {
     const [state, setState] = useImmer<State>({
         userId: id !== NEW ? parseInt(id) : 0,
         user: defaultUser,
-        allClients: [],
         selectedClient: none
     });
 
+    const loadUser = async () => {
+        const user = pipe(
+            await getUserClients(state.userId),
+            getOrElse((): UserClientsType => defaultUser)
+        );
+        setState((draft) => {
+            draft.user = user;
+        });
+    };
+
     useEffect(() => {
-        const loadUser = async () => {
-            const user = pipe(
-                await getUserClients(state.userId),
-                getOrElse((): UserClients => defaultUser)
-            );
-            setState((draft) => {
-                draft.user = user;
-            });
-        };
-
-        const loadAllClients = async () => {
-            const allClients = pipe(
-                await getAllClients(),
-                map((res: ClientListResponse) => res.clients),
-                getOrElse((): Array<ClientListItem> => [])
-            );
-            setState((draft) => {
-                draft.allClients = allClients;
-            });
-        };
-
         loadUser();
-        loadAllClients();
-    }, [state.userId, setState]);
+    }, [loadUser]);
 
     const selectClient = (client: UserClient) =>
         setState((draft) => {
             draft.selectedClient = some(client);
         });
-
-    const clientOptions: Array<SelectOption<number>> = useMemo(() =>
-            state.allClients
-                .filter((client) => !state.user.clients.find((otherClient) => client.id === otherClient.id))
-                .sort((client1, client2) => client1.name.localeCompare(client2.name))
-                .map((client) => ({
-                    label: client.name,
-                    value: client.id
-                })),
-        [state.allClients, state.user.clients]);
-
-    const goToClient = (clientId: number) => {
-        // TODO finish this
-    };
-
-    const removeClientClick = (clientId: number) => {
-        // TODO finish this
-    };
-
-    const clientItems: Array<Item> = state.user.clients.map((client) => ({
-        click: () => selectClient(client),
-        avatar: () => <Business />,
-        text: {
-            primary: client.name
-        },
-        secondaryActions: [
-            {
-                text: 'Go',
-                click: () => goToClient(client.id)
-            },
-            {
-                text: 'Remove',
-                click: () => removeClientClick(client.id)
-            }
-        ],
-        active: exists((selected: UserClient) => selected.id === client.id)(state.selectedClient)
-    }));
 
     return (
         <div className="UserGrants">
@@ -131,7 +81,13 @@ const UserGrants = (props: Props) => {
                     item
                     md={ 5 }
                 >
-                    <SectionHeader title="Clients" />
+                    <UserClients
+                        userClients={ state.user.clients }
+                        userId={ state.userId }
+                        updateClients={ loadUser }
+                        selectedClient={ state.selectedClient }
+                        selectClient={ selectClient }
+                    />
                 </Grid>
                 <Grid
                     direction="column"
