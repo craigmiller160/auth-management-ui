fcd20fec236b4c74d281afcf880abd71234d50ba
Craig Miller <craigmiller160@gmail.com>
2020-08-30 14:53:31 -0400

    Began adding code for navigation drawer

--- src/components/Root/Navbar/index.tsx
+++ src/components/Root/Navbar/index.tsx
@@ -11,15 +11,40 @@ import { RootState } from '../../../store';
 import { login, logout } from '../../../services/AuthService';
 import authSlice from '../../../store/auth/slice';
 import { none } from 'fp-ts/es6/Option';
+import useTheme from '@material-ui/core/styles/useTheme';
+import { useMediaQuery } from '@material-ui/core';
+import { useImmer } from 'use-immer';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+interface State {
+    menuOpen: boolean;
+}
 
 const isActive = (pathname: string, path: string, exact: boolean = false): boolean =>
     exact ? pathname === path : pathname !== '/' && pathname.startsWith(path);
 
 const Navbar = () => {
+    const theme = useTheme();
+    const isNotPhone = useMediaQuery(theme.breakpoints.up('md'));
     const dispatch = useDispatch();
     const location = useLocation();
     const isAuth = useSelector(isAuthorized);
     const hasChecked = useSelector((state: RootState) => state.auth.hasChecked);
+    const [state, setState] = useImmer<State>({
+        menuOpen: false
+    });
+
+    const handleMenuOpen = () =>
+        setState((draft) => {
+            draft.menuOpen = true;
+        });
+
+    const handleMenuClose = () =>
+        setState((draft) => {
+            draft.menuOpen = false;
+        });
+
     const authBtnText = isAuth ? 'Logout' : 'Login';
 
     const doLogout = async () => {
@@ -34,6 +59,12 @@ const Navbar = () => {
     return (
         <AppBar position="static" className="Navbar">
             <Toolbar>
+                {
+                    !isNotPhone &&
+                    <IconButton edge="start" color="inherit" onClick={ handleMenuOpen }>
+                        <MenuIcon />
+                    </IconButton>
+                }
                 <Button variant="text" color="inherit">
                     <NavLink to="/" exact className="NavLink">
                         <Typography variant="h6" noWrap>OAuth Management</Typography>
