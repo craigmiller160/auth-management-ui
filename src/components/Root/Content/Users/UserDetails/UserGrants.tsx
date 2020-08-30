2a039ac1759f1b2016e55f7472d7dffd3f96e725
Craig Miller <craigmiller160@gmail.com>
2020-08-30 12:25:14 -0400

    Added loading of User Clients

--- src/components/Root/Content/Users/UserDetails/UserGrants.tsx
+++ src/components/Root/Content/Users/UserDetails/UserGrants.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { match } from 'react-router';
+import { useImmer } from 'use-immer';
+import { pipe } from 'fp-ts/es6/pipeable';
+import { getUserClients } from '../../../../../services/UserService';
+import { getOrElse } from 'fp-ts/es6/Either';
+import { UserClients } from '../../../../../types/user';
 
+interface State {
+    userId: number;
+    user: UserClients;
+}
+const NEW = 'new';
+interface MatchParams {
+    id: string;
+}
 interface Props {
-
+    match: match<MatchParams>;
 }
 
+const defaultUser: UserClients = {
+    id: 0,
+    clients: []
+};
+
 const UserGrants = (props: Props) => {
+    const id = props.match.params.id;
+    const [state, setState] = useImmer<State>({
+        userId: id !== NEW ? parseInt(id) : 0,
+        user: defaultUser
+    });
+
+    useEffect(() => {
+        const action = async () => {
+            const user = pipe(
+                await getUserClients(state.userId),
+                getOrElse((): UserClients => defaultUser)
+            );
+            setState((draft) => {
+                draft.user = user;
+            });
+        };
+
+        if (state.userId > 0) {
+            action();
+        }
+    }, [state.userId, setState]);
+
     return (
         <div>
             <h1>User Grants</h1>
