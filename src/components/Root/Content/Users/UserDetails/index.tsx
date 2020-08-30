1acdc61de71c5014eb63ee23e0f07e26be9c62df
Craig Miller <craigmiller160@gmail.com>
2020-08-30 10:19:31 -0400

    Added tabs

--- src/components/Root/Content/Users/UserDetails/index.tsx
+++ src/components/Root/Content/Users/UserDetails/index.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { useRouteMatch } from 'react-router';
 import { useImmer } from 'use-immer';
 import { PageHeader } from '../../../../ui/Header';
+import { Tabs } from '@material-ui/core';
+import Tab from '@material-ui/core/Tab';
+
+const TAB_CONFIG = 0;
+const TAB_GRANTS = 1;
+const TAB_AUTHS = 2;
 
 interface State {
     userId: number;
+    selectedTab: number;
 }
 
 interface MatchParams {
@@ -16,12 +23,23 @@ const UserDetails = () => {
     const match = useRouteMatch<MatchParams>();
     const id = match.params.id;
     const [state, setState] = useImmer<State>({
-        userId: id !== NEW ? parseInt(id) : 0
+        userId: id !== NEW ? parseInt(id) : 0,
+        selectedTab: TAB_CONFIG
     });
 
     return (
         <div className="UserDetails">
             <PageHeader title="User Details" />
+            <Tabs
+                value={ state.selectedTab }
+                indicatorColor="primary"
+                textColor="primary"
+                centered
+            >
+                <Tab label="Config" />
+                <Tab label="Grants" />
+                <Tab label="Authentications" />
+            </Tabs>
         </div>
     );
 }
