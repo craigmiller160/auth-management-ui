dfec6e3c5ae8dd4a688297bed3b889b80a716f54
Craig Miller <craigmiller160@gmail.com>
2020-08-30 15:50:39 -0400

    Added generic Switch component

---
+++ src/components/ui/Form/Switch.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Control, Controller } from 'react-hook-form';
+import { FieldName } from 'react-hook-form/dist/types/form';
+import MuiSwitch from '@material-ui/core/Switch';
+import { FormControlLabel } from '@material-ui/core';
+
+interface Props<T> {
+    name: keyof T;
+    control: Control<T>;
+    label: string;
+    color?: 'primary' | 'secondary' | 'default';
+}
+
+const Switch = <T extends object>(props: Props<T>) => {
+    const {
+        name,
+        control,
+        color,
+        label
+    } = props;
+
+    return (
+        <Controller
+            control={ control }
+            name={ name as FieldName<T> }
+            render={ ({ onChange, onBlur, value }) => (
+                <FormControlLabel
+                    label={ label }
+                    control={
+                        <MuiSwitch
+                            onChange={ (event) => onChange(event.target.checked) }
+                            onBlur={ onBlur }
+                            checked={ value }
+                            color={ color ?? 'primary' }
+                        />
+                    }
+                />
+            ) }
+        />
+    );
+};
+
+export default Switch;
