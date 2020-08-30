c82392bb9e3f54da45ee4cb075aa0414a1c86230
Craig Miller <craigmiller160@gmail.com>
2020-08-30 15:07:04 -0400

    Added all links for the mobile menu

--- src/components/Root/Navbar/MobileMenu.tsx
+++ src/components/Root/Navbar/MobileMenu.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Drawer } from '@material-ui/core';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import './MobileMenu.scss';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 interface Props {
     menuOpen: boolean;
@@ -10,6 +12,8 @@ interface Props {
 }
 
 const MobileMenu = (props: Props) => {
+    const history = useHistory();
+
     return (
         <Drawer
             className="MobileMenu"
@@ -30,6 +34,28 @@ const MobileMenu = (props: Props) => {
                     OAuth Management
                 </Typography>
             </NavLink>
+            <ListItem
+                className="item"
+                onClick={ props.handleMenuClose }
+            >
+                <NavLink
+                    to="/users"
+                    className="NavLink"
+                >
+                    <ListItemText>Users</ListItemText>
+                </NavLink>
+            </ListItem>
+            <ListItem
+                className="item"
+                onClick={ props.handleMenuClose }
+            >
+                <NavLink
+                    to="/clients"
+                    className="NavLink"
+                >
+                    <ListItemText>Clients</ListItemText>
+                </NavLink>
+            </ListItem>
         </Drawer>
     );
 };
