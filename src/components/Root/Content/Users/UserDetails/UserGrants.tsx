b357d9a20f09a7dfceb0eb24335b7b0ef150a4b2
Craig Miller <craigmiller160@gmail.com>
2020-08-30 17:44:10 -0400

    Began building the UserGrants page

--- src/components/Root/Content/Users/UserDetails/UserGrants.tsx
+++ src/components/Root/Content/Users/UserDetails/UserGrants.tsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { match } from 'react-router';
 import { useImmer } from 'use-immer';
 import { pipe } from 'fp-ts/es6/pipeable';
 import { getUserClients } from '../../../../../services/UserService';
-import { getOrElse } from 'fp-ts/es6/Either';
-import { UserClients } from '../../../../../types/user';
+import { getOrElse, map } from 'fp-ts/es6/Either';
+import { UserClient, UserClients } from '../../../../../types/user';
 import { Grid, Typography } from '@material-ui/core';
 import './UserGrants.scss';
+import { SectionHeader } from '../../../../ui/Header';
+import { ClientListItem, ClientListResponse } from '../../../../../types/client';
+import { getAllClients } from '../../../../../services/ClientService';
+import { SelectOption } from '../../../../ui/Form/Autocomplete';
+import { Item } from '../../../../ui/List';
+import { Business } from '@material-ui/icons';
+import { exists, none, Option, some } from 'fp-ts/es6/Option';
 
 interface State {
     userId: number;
     user: UserClients;
+    allClients: Array<ClientListItem>;
+    selectedClient: Option<UserClient>;
 }
 const NEW = 'new';
 interface MatchParams {
@@ -30,11 +39,13 @@ const UserGrants = (props: Props) => {
     const id = props.match.params.id;
     const [state, setState] = useImmer<State>({
         userId: id !== NEW ? parseInt(id) : 0,
-        user: defaultUser
+        user: defaultUser,
+        allClients: [],
+        selectedClient: none
     });
 
     useEffect(() => {
-        const action = async () => {
+        const loadUser = async () => {
             const user = pipe(
                 await getUserClients(state.userId),
                 getOrElse((): UserClients => defaultUser)
@@ -44,10 +55,62 @@ const UserGrants = (props: Props) => {
             });
         };
 
-        if (state.userId > 0) {
-            action();
-        }
+        const loadAllClients = async () => {
+            const allClients = pipe(
+                await getAllClients(),
+                map((res: ClientListResponse) => res.clients),
+                getOrElse((): Array<ClientListItem> => [])
+            );
+            setState((draft) => {
+                draft.allClients = allClients;
+            });
+        };
+
+        loadUser();
+        loadAllClients();
     }, [state.userId, setState]);
+
+    const selectClient = (client: UserClient) =>
+        setState((draft) => {
+            draft.selectedClient = some(client);
+        });
+
+    const clientOptions: Array<SelectOption<number>> = useMemo(() =>
+            state.allClients
+                .filter((client) => !state.user.clients.find((otherClient) => client.id === otherClient.id))
+                .sort((client1, client2) => client1.name.localeCompare(client2.name))
+                .map((client) => ({
+                    label: client.name,
+                    value: client.id
+                })),
+        [state.allClients, state.user.clients]);
+
+    const goToClient = (clientId: number) => {
+        // TODO finish this
+    };
+
+    const removeClientClick = (clientId: number) => {
+        // TODO finish this
+    };
+
+    const clientItems: Array<Item> = state.user.clients.map((client) => ({
+        click: () => selectClient(client),
+        avatar: () => <Business />,
+        text: {
+            primary: client.name
+        },
+        secondaryActions: [
+            {
+                text: 'Go',
+                click: () => goToClient(client.id)
+            },
+            {
+                text: 'Remove',
+                click: () => removeClientClick(client.id)
+            }
+        ],
+        active: exists((selected: UserClient) => selected.id === client.id)(state.selectedClient)
+    }));
 
     return (
         <div className="UserGrants">
@@ -62,7 +125,22 @@ const UserGrants = (props: Props) => {
                 direction="row"
                 justify="space-around"
             >
-
+                <Grid
+                    direction="column"
+                    container
+                    item
+                    md={ 5 }
+                >
+                    <SectionHeader title="Clients" />
+                </Grid>
+                <Grid
+                    direction="column"
+                    container
+                    item
+                    md={ 5 }
+                >
+                    <SectionHeader title="Roles" />
+                </Grid>
             </Grid>
         </div>
     );
