7e4d274569b4bd02d1a8cf62b8d7767f82fa160a
Craig Miller <craigmiller160@gmail.com>
2020-09-08 13:51:51 -0400

    More work on InputDialog

--- src/components/ui/Dialog/InputDialog.tsx
+++ src/components/ui/Dialog/InputDialog.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { MouseEvent, useEffect } from 'react';
 import BaseDialog, { DialogAction } from './BaseDialog';
 import { useForm } from 'react-hook-form';
+import TextField from '../Form/TextField';
 
 interface InputForm {
     value: string;
@@ -9,17 +10,43 @@ interface InputForm {
 interface Props {
     open: boolean;
     title: string;
+    onCancel: (event: MouseEvent<HTMLButtonElement>) => void;
+    onSave: (value: string) => void;
+    label: string;
+    initialValue?: string;
 }
 
 const InputDialog = (props: Props) => {
     const {
         open,
-        title
+        title,
+        onCancel,
+        onSave,
+        label,
+        initialValue = ''
     } = props;
 
+    const { control, handleSubmit, errors, reset } = useForm<InputForm>({
+        mode: 'onBlur',
+        reValidateMode: 'onChange',
+        defaultValues: {
+            value: initialValue
+        }
+    });
+
+    useEffect(() => {
+        reset({
+            value: initialValue
+        });
+    }, [open, initialValue]);
+
+    const onSubmit = (values: InputForm) => {
+        // TODO handle this
+    };
+
     const actions: Array<DialogAction> = [
-        { label: 'Save', onClick: () => {} },
-        { label: 'Cancel', onClick: () => {} }
+        { label: 'Save', onClick: handleSubmit(onSubmit) },
+        { label: 'Cancel', onClick: onCancel }
     ];
 
     return (
@@ -28,7 +55,13 @@ const InputDialog = (props: Props) => {
             title={ title }
             actions={ actions }
         >
-
+            <TextField
+                name="value"
+                control={ control }
+                label={ label }
+                error={ errors.value }
+                rules={ { required: 'Required' } }
+            />
         </BaseDialog>
     );
 };
