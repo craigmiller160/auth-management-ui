0778b4f4d47630aba52ddb4d1741d07db9b749ca
Craig Miller <craigmiller160@gmail.com>
2020-09-12 15:58:41 -0400

    Began building out the authorizations page

--- src/components/Root/Content/Clients/ClientDetails/ClientAuths.tsx
+++ src/components/Root/Content/Clients/ClientDetails/ClientAuths.tsx
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { match } from 'react-router';
+import { UserAuthDetails } from '../../../../../types/user';
+import { useImmer } from 'use-immer';
+import { pipe } from 'fp-ts/es6/pipeable';
+import { getAuthDetailsForClient } from '../../../../../services/ClientService';
+import { map } from 'fp-ts/es6/Either';
+import { Grid, Typography } from '@material-ui/core';
+import './ClientAuths.scss';
+import List, { Item } from '../../../../ui/List';
 
-const ClientAuths = () => {
+const NEW = 'new'; // TODO create a common constant for this
+interface MatchParams { // TODO create a common type for this...
+    id: string;
+}
+
+interface Props {
+    match: match<MatchParams>; // TODO create a common type for this
+}
+
+interface State {
+    clientId: number;
+    clientName: string;
+    userAuthDetails: Array<UserAuthDetails>;
+}
+
+const ClientAuths = (props: Props) => {
+    const id = props.match.params.id;
+    const [state, setState] = useImmer<State>({
+        clientId: id !== NEW ? parseInt(id) : 0, // TODO make re-usable function
+        clientName: '',
+        userAuthDetails: []
+    });
+
+    const loadAuthDetails = async () =>
+        pipe(
+            await getAuthDetailsForClient(state.clientId),
+            map((clientAuthDetails) => {
+                setState((draft) => {
+                    draft.clientName = clientAuthDetails.clientName;
+                    draft.userAuthDetails = clientAuthDetails.userAuthDetails;
+                });
+            })
+        );
+
+    useEffect(() => {
+        loadAuthDetails();
+    }, []);
+
+    const items: Array<Item> = [];
+
     return (
-        <div>
-            <h1>Client Auths</h1>
+        <div className="ClientAuths">
+            <Typography
+                className="name"
+                variant="h5"
+            >
+                { state.clientName }
+            </Typography>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+            >
+                <Grid
+                    item
+                    md={ 5 }
+                >
+                    {
+                        items.length > 0 &&
+                        <List items={ items } />
+                    }
+                    {
+                        items.length === 0 &&
+                        <Typography
+                            className="no-auths"
+                            variant="body1"
+                        >
+                            No Authorizations
+                        </Typography>
+                    }
+                </Grid>
+            </Grid>
         </div>
     );
 };
