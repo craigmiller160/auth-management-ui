b59f0255ccd406b1b0479f5fe3f6743b122ffc8e
Craig Miller <craigmiller160@gmail.com>
2020-09-13 17:11:25 -0400

    Began laying out ClientGrants page

--- src/components/Root/Content/Clients/ClientDetails/ClientGrants.tsx
+++ src/components/Root/Content/Clients/ClientDetails/ClientGrants.tsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { IdMatchProps, NEW_ID } from '../../../../../types/detailsPage';
+import { useImmer } from 'use-immer';
+import { pipe } from 'fp-ts/es6/pipeable';
+import { getFullClientDetails } from '../../../../../services/ClientService';
+import { map } from 'fp-ts/es6/Either';
+import { ClientRole, ClientUser } from '../../../../../types/client';
+import { Typography } from '@material-ui/core';
+import './ClientGrants.scss';
 
-const ClientGrants = () => {
+interface Props extends IdMatchProps {}
+
+interface State {
+    clientId: number;
+    clientName: string;
+    allRoles: Array<ClientRole>;
+    users: Array<ClientUser>;
+}
+
+const ClientGrants = (props: Props) => {
+    const id = props.match.params.id;
+
+    const [state, setState] = useImmer<State>({
+        clientId: id !== NEW_ID ? parseInt(id) : 0,
+        clientName: '',
+        allRoles: [],
+        users: []
+    });
+
+    const loadFullClientDetails = async () => {
+        pipe(
+            await getFullClientDetails(state.clientId),
+            map((fullClientDetails) =>
+                setState((draft) => {
+                    draft.clientName = fullClientDetails.name;
+                    draft.allRoles = fullClientDetails.roles;
+                    draft.users = fullClientDetails.users;
+                })
+            )
+        )
+    };
+
+    useEffect(() => {
+        loadFullClientDetails();
+    }, []);
+
+    console.log(state.users); // TODO delete this
+
     return (
-        <div>
-            <h1>Client Grants</h1>
+        <div className="ClientGrants">
+            <Typography
+                className="name"
+                variant="h5"
+            >
+                { state.clientName }
+            </Typography>
         </div>
     );
 };
