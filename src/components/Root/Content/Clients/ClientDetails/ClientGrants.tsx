862a217eb134243fe9dfe6edb8332cf84fe3e0de
Craig Miller <craigmiller160@gmail.com>
2020-09-13 18:39:45 -0400

    Implemented some more logic around showing users and roles

--- src/components/Root/Content/Clients/ClientDetails/ClientGrants.tsx
+++ src/components/Root/Content/Clients/ClientDetails/ClientGrants.tsx
@@ -5,12 +5,13 @@ import { pipe } from 'fp-ts/es6/pipeable';
 import { getFullClientDetails } from '../../../../../services/ClientService';
 import { Either, flatten, getOrElse, map } from 'fp-ts/es6/Either';
 import { ClientRole, ClientUser, FullClientDetails } from '../../../../../types/client';
-import { Grid, Typography } from '@material-ui/core';
+import { Button, Grid, Typography } from '@material-ui/core';
 import './ClientGrants.scss';
 import { getAllUsers } from '../../../../../services/UserService';
 import { UserDetails, UserList } from '../../../../../types/user';
 import List, { Item } from '../../../../ui/List';
 import PersonIcon from '@material-ui/icons/Person';
+import { exists, map as oMap, none, Option, some, getOrElse as oGetOrElse } from 'fp-ts/es6/Option';
 
 interface Props extends IdMatchProps {}
 
@@ -20,6 +21,7 @@ interface State {
     allRoles: Array<ClientRole>;
     clientUsers: Array<ClientUser>;
     allUsers: Array<UserDetails>;
+    selectedUser: Option<ClientUser>;
 }
 
 
@@ -31,7 +33,8 @@ const ClientGrants = (props: Props) => {
         clientName: '',
         allRoles: [],
         clientUsers: [],
-        allUsers: []
+        allUsers: [],
+        selectedUser: none
     });
 
     const loadFullClientDetails = async (): Promise<Array<ClientUser>> =>
@@ -73,6 +76,10 @@ const ClientGrants = (props: Props) => {
     const userItems: Array<Item> = state.clientUsers
         .map((user) => ({
             avatar: () => <PersonIcon />,
+            click: () => setState((draft) => {
+                draft.selectedUser = some(user);
+            }),
+            active: exists((selected: ClientUser) => selected.id === user.id)(state.selectedUser),
             text: {
                 primary: `${user.firstName} ${user.lastName}`,
                 secondary: user.roles.map((role) => role.name).join(', ')
@@ -118,7 +125,27 @@ const ClientGrants = (props: Props) => {
                     >
                         Users
                     </Typography>
-                    <List items={ userItems } />
+                    {
+                        userItems.length > 0 &&
+                        <List items={ userItems } />
+                    }
+                    {
+                        userItems.length === 0 &&
+                        <Typography
+                            className="no-items"
+                            variant="body1"
+                        >
+                            No Users
+                        </Typography>
+                    }
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={ () => {} }
+                        disabled={ state.allUsers.length === 0 }
+                    >
+                        Add User
+                    </Button>
                 </Grid>
                 <Grid item md={ 2 } />
                 <Grid
@@ -127,7 +154,58 @@ const ClientGrants = (props: Props) => {
                     item
                     md={ 5 }
                 >
+                    <Typography
+                        className="name"
+                        variant="body1"
+                    >
+                        Roles
+                    </Typography>
+                    {
+                        pipe(
+                            state.selectedUser,
+                            oMap((selectedUser: ClientUser) => {
+                                const roleItems: Array<Item> = selectedUser.roles
+                                    .map((role) => ({
+                                        text: {
+                                            primary: 'ABC'
+                                        }
+                                    }));
 
+                                const availableRoles = state.allRoles
+                                    .filter((role) => {
+                                        const index = selectedUser.roles.findIndex((uRole) => uRole.id === role.id);
+                                        return index === -1;
+                                    });
+
+                                return (
+                                    <>
+                                        {
+                                            roleItems.length > 0 &&
+                                            <List items={ roleItems } />
+                                        }
+                                        {
+                                            roleItems.length === 0 &&
+                                            <Typography
+                                                className="no-items"
+                                                variant="body1"
+                                            >
+                                                No Roles
+                                            </Typography>
+                                        }
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            onClick={ () => {} }
+                                            disabled={ availableRoles.length === 0 }
+                                        >
+                                            Add Role
+                                        </Button>
+                                    </>
+                                );
+                            }),
+                            oGetOrElse(() => <div />)
+                        )
+                    }
                 </Grid>
             </Grid>
         </div>
