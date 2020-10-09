1d76f13242b1cfccf7e3e3acd76317fb196e912b
Craig Miller <craigmiller160@gmail.com>
2020-10-09 19:44:40 -0400

    Deleted mock stuff

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -27,9 +27,7 @@
 import loginPage from './commands/loginPage';
 import navbar from './commands/navbar';
 import homePage from './commands/homePage';
-import mockApis from './commands/mockApis';
 
 Cypress.Commands.add('loginPage', loginPage);
 Cypress.Commands.add('navbar', navbar);
 Cypress.Commands.add('homePage', homePage);
-Cypress.Commands.add('mockApis', mockApis);
