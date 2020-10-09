1ea27f07ac0c6deac64d7dd03b4967f2669c298c
Craig Miller <craigmiller160@gmail.com>
2020-10-09 16:20:23 -0400

    Initialized server for mocking

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,9 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+beforeEach(() => {
+    if(Cypress.env('MOCK') === 'true') {
+        cy.server();
+    }
+});
