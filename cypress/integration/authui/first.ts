1fd71fee86d2f565bd1a42f9e54a82ad71e5bc89
Craig Miller <craigmiller160@gmail.com>
2020-10-09 14:56:09 -0400

    Added LoginPageCommands

--- cypress/integration/authui/first.ts
+++ cypress/integration/authui/first.ts
@@ -27,13 +27,13 @@ describe('First Test', () => {
         cy.get('button#navbar-auth-btn')
             .should('have.text', 'Login')
             .click();
-        cy.get('#oauth2-login-page #username')
-            .should('be.visible')
-            .type('craig@gmail.com');
-        cy.get('#oauth2-login-page #password')
-            .type('password');
-        cy.get('#oauth2-login-page #submitBtn')
-            .click();
+        // cy.get('#oauth2-login-page #username')
+        //     .should('be.visible')
+        //     .type('craig@gmail.com');
+        // cy.get('#oauth2-login-page #password')
+        //     .type('password');
+        // cy.get('#oauth2-login-page #submitBtn')
+        //     .click();
         cy.get('button#navbar-auth-btn')
             .should('have.text', 'Logout');
     });
