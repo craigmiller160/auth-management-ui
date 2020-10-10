ec1bfc826e46c6c060490ce964b32c0dd9477fed
Craig Miller <craigmiller160@gmail.com>
2020-10-09 20:50:07 -0400

    Setup clients page

---
+++ cypress/integration/authui/clients.ts
@@ -0,0 +1,43 @@
+/*
+ *     Auth Management UI
+ *     Copyright (C) 2020 Craig Miller
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+describe('Clients', () => {
+    it('New Client', () => {
+        throw new Error();
+    });
+
+    it('Edit Client Config', () => {
+        throw new Error();
+    });
+
+    it('Delete Client', () => {
+        throw new Error();
+    });
+
+    it('Add/Edit/Delete client roles', () => {
+        throw new Error();
+    });
+
+    it('Add/Remove user grants & roles', () => {
+        throw new Error();
+    });
+
+    it('View/Remove user authentications', () => {
+        throw new Error();
+    });
+});
