fb49519faab022be3ac6a5baeb2b9ddf04567116
Craig Miller <craigmiller160@gmail.com>
2020-11-13 14:30:07 -0500

    Cleaned up import

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import '@craigmiller160/web-config/polyfills'; // TODO see if this can be added through a separate webpack entrypoint
+import '@craigmiller160/web-config/polyfills';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
