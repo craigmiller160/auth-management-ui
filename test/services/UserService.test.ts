e4dfa2b1c0de6429df82873a1f669a856aad061b
Craig Miller <craigmiller160@gmail.com>
2021-01-02 13:05:15 -0500

    Stubbed out more tests

---
+++ test/services/UserService.test.ts
@@ -0,0 +1,69 @@
+/*
+ *     Auth Management UI
+ *     Copyright (C) 2020 Craig Miller
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import MockAdapter from 'axios-mock-adapter';
+
+describe('UserService', () => {
+    it('getAllUsers', () => {
+        throw new Error();
+    });
+
+    it('getUserDetails', () => {
+        throw new Error();
+    });
+
+    it('getUserClients', () => {
+        throw new Error();
+    });
+
+    it('updateUser', () => {
+        throw new Error();
+    });
+
+    it('createUser', () => {
+        throw new Error();
+    });
+
+    it('deleteUser', () => {
+        throw new Error();
+    });
+
+    it('removeClientFromUser', () => {
+        throw new Error();
+    });
+
+    it('addClientToUser', () => {
+        throw new Error();
+    });
+
+    it('removeRoleFromUser', () => {
+        throw new Error();
+    });
+
+    it('addRoleToUser', () => {
+        throw new Error();
+    });
+
+    it('getAllUserAuthDetails', () => {
+        throw new Error();
+    });
+
+    it('revokeUserAuthAccess', () => {
+        throw new Error();
+    });
+});
