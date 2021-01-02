5238c168beca8d73ca82d8ccf809b45a0b019fa4
Craig Miller <craigmiller160@gmail.com>
2021-01-02 13:00:11 -0500

    Stubbed out some tests

---
+++ test/services/ClientService.test.ts
@@ -0,0 +1,65 @@
+/*
+ *     Auth Management UI
+ *     Copyright (C) 2020 Craig Miller
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import MockAdapter from 'axios-mock-adapter';
+
+describe('ClientService', () => {
+    it('getAllClients', () => {
+        throw new Error();
+    });
+
+    it('getFullClientDetails', () => {
+        throw new Error();
+    });
+
+    it('getClientDetails', () => {
+        throw new Error();
+    });
+
+    it('getClientWithRoles', () => {
+        throw new Error();
+    });
+
+    it('updateClient', () => {
+        throw new Error();
+    });
+
+    it('createClient', () => {
+        throw new Error();
+    });
+
+    it('deleteClient', () => {
+        throw new Error();
+    });
+
+    it('generateGuid', () => {
+        throw new Error();
+    });
+
+    it('removeUserFromClient', () => {
+        throw new Error();
+    });
+
+    it('addUserToClient', () => {
+        throw new Error();
+    });
+
+    it('getAuthDetailsForClient', () => {
+        throw new Error();
+    });
+});
