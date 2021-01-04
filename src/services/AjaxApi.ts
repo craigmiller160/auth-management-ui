f381ac5af2a85a5f08be1737ea102ea576569b2e
Craig <craigmiller160@gmail.com>
2021-01-04 16:03:48 -0500

    Integrated new error handler into API

--- src/services/AjaxApi.ts
+++ src/services/AjaxApi.ts
@@ -1,6 +1,8 @@
 import { createApi } from '@craigmiller160/ajax-api-fp-ts';
+import ajaxErrorHandler from './ajaxErrorHandler';
 
 export default createApi({
     baseURL: '/auth-manage-ui/api',
-    useCsrf: true
+    useCsrf: true,
+    defaultErrorHandler: ajaxErrorHandler
 });
