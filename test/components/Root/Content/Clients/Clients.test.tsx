a4564a510c35870e3400a2c22e514d0457d0e4f0
Craig Miller <craigmiller160@gmail.com>
2021-01-07 09:08:37 -0500

    Starting work on tests

---
+++ test/components/Root/Content/Clients/Clients.test.tsx
@@ -0,0 +1,76 @@
+/*
+ *     Auth Management UI
+ *     Copyright (C) 2020 Craig Miller
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+import { mount, ReactWrapper } from 'enzyme';
+import MockAdapter from 'axios-mock-adapter';
+import { GraphQLQueryResponse } from '@craigmiller160/ajax-api-fp-ts';
+import { act } from 'react-dom/test-utils';
+import ajaxApi from '../../../../../src/services/AjaxApi';
+import Clients from '../../../../../src/components/Root/Content/Clients';
+import { ClientListResponse } from '../../../../../src/types/client';
+import { createTestComponent, createTestRouter, RouterOptions } from '@craigmiller160/react-test-utils';
+
+const mockApi = new MockAdapter(ajaxApi.instance);
+
+const defaultRouterOptions: RouterOptions = {
+    initialEntries: [ '/' ],
+    initialIndex: 0
+};
+
+const TestRouter = createTestRouter(defaultRouterOptions);
+const TestClients = createTestComponent({}, Clients);
+
+// TODO make this trick re-usable
+const doMount = async (): Promise<ReactWrapper> => {
+    let component: any;
+    await act(async () => {
+        component = await mount(
+            <TestRouter>
+                <TestClients />
+            </TestRouter>
+        )
+    });
+    return component as ReactWrapper;
+};
+
+describe('Clients', () => {
+    beforeEach(() => {
+        mockApi.reset();
+    });
+
+    describe('rendering', () => {
+        it('renders', async () => {
+            const response: GraphQLQueryResponse<ClientListResponse> = {
+                data: {
+                    clients: [
+                        {
+                            id: 1,
+                            name: 'Client',
+                            clientKey: 'key'
+                        }
+                    ]
+                }
+            };
+            mockApi.onPost('/graphql')
+                .reply(200, response);
+            const component = await doMount();
+            console.log(component.debug());
+        });
+    });
+});
