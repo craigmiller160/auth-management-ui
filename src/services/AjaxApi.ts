16ac4875f8a7d2ff741c8d2b93715abe3e2f1c3b
Craig Miller <craigmiller160@gmail.com>
2021-01-24 13:28:45 -0500

    Fixed api path prefix

--- src/services/AjaxApi.ts
+++ src/services/AjaxApi.ts
@@ -2,7 +2,7 @@ import { createApi } from '@craigmiller160/ajax-api-fp-ts';
 import ajaxErrorHandler from './ajaxErrorHandler';
 
 export default createApi({
-    baseURL: '/auth-manage-ui/api',
+    baseURL: '/auth-management/api',
     useCsrf: true,
     defaultErrorHandler: ajaxErrorHandler
 });
