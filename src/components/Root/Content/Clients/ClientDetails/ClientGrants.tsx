55e9101592e6fa90c3ed00825da9cccb3bb1db0c
Craig Miller <craigmiller160@gmail.com>
2021-05-03 16:36:56 -0400

    Feature/auth 36 (#30)
    * Fixed intelllisense issue
    
    * Stubbed out tests
    
    * Preparing to write tests for broken ClientGrants page
    
    * Beginning to implement test logic to identify and fix broken parts
    
    * Experimenting with identifying the solution
    
    * Got some loading working
    
    * More improvements to test logic
    
    * More cleanup for loading logic
    
    * Finished first rendering test
    
    * Implemented another test
    
    * Finished all rendering tests
    
    * Another test
    
    * Fixed linting errors
    
    * Implemented more test logic
    
    * More test progress
    
    * More test progress
    
    * Finished another test
    
    * Implementing more tests
    
    * Committing progress
    
    * Implemented more test logic
    
    * More test improvements
    
    * Got another test working
    
    * Implemented another test
    
    * More work on the tests
    
    * Wrote more test logic
    
    * More work on test
    
    * Making more progress on tests
    
    * Some progress identifying problems in test
    
    * Finished another test
    
    * More fp-ts refactoring
    
    * Refactored another function
    
    * Starting to implement more tests
    
    * Finished another test
    
    * Implemented another test
    
    * Implemented another test
    
    * Implemented another test
    
    * Finished the last test
    
    * Fixed linting errors

--- src/components/Root/Content/Clients/ClientDetails/ClientGrants.tsx
+++ src/components/Root/Content/Clients/ClientDetails/ClientGrants.tsx
@@ -21,7 +21,6 @@ import { useImmer } from 'use-immer';
 import { pipe } from 'fp-ts/es6/pipeable';
 import Grid from '@material-ui/core/Grid';
 import * as TE from 'fp-ts/es6/TaskEither';
-import * as T from 'fp-ts/es6/Task';
 import {
 	fromNullable,
 	getOrElse as oGetOrElse,
@@ -48,7 +47,7 @@ import {
 	getAllUsers,
 	removeRoleFromUser
 } from '../../../../../services/UserService';
-import { UserDetails, UserList } from '../../../../../types/user';
+import { UserDetails } from '../../../../../types/user';
 import ClientGrantUsers from './ClientGrantUsers';
 import ClientGrantRoles from './ClientGrantRoles';
 
@@ -63,6 +62,11 @@ interface State {
 	selectedUser: Option<ClientUser>;
 }
 
+interface LoadingContainer {
+	client: FullClientDetails;
+	allUsers: UserDetails[];
+}
+
 const ClientGrants = (props: Props): JSX.Element => {
 	const { id } = props.match.params;
 
@@ -75,133 +79,113 @@ const ClientGrants = (props: Props): JSX.Element => {
 		selectedUser: none
 	});
 
-	const loadFullClientDetails = useCallback(
-		(): T.Task<Array<ClientUser>> =>
-			pipe(
-				getFullClientDetails(state.clientId),
-				TE.map(
-					(
-						fullClientDetails: FullClientDetails
-					): Array<ClientUser> => {
-						setState((draft) => {
-							draft.clientName = fullClientDetails.name;
-							draft.allRoles = fullClientDetails.roles;
-							draft.clientUsers = fullClientDetails.users;
-						});
-						return fullClientDetails.users;
-					}
-				),
-				TE.getOrElse((): T.Task<Array<ClientUser>> => T.of([]))
-			),
-		[state.clientId, setState]
-	);
-
-	const loadUsers = useCallback(
-		(clientUsers: Array<ClientUser>) =>
-			pipe(
-				getAllUsers(),
-				TE.map((list: UserList) => list.users),
-				TE.map((
-					users: UserDetails[] // TODO combine with above function
-				) =>
-					users.filter((user) => {
-						const index = clientUsers.findIndex(
-							(cUser) => cUser.id === user.id
-						);
-						return index === -1;
-					})
-				),
-				TE.map((users: UserDetails[]) =>
-					setState((draft) => {
-						// TODO see if this can be moved to the end
-						draft.allUsers = users;
-					})
-				)
-			),
-		[setState]
-	);
-
-	// TODO this one is an absolute mess, try to figure out how to optimize this if it works
-	const loadAll = useCallback(
+	const loadEverything = useCallback(
 		() =>
 			pipe(
-				loadFullClientDetails(),
-				T.map((clientUsers: Array<ClientUser>) => {
-					loadUsers(clientUsers)();
+				getFullClientDetails(state.clientId),
+				TE.chain((clientDetails) =>
+					pipe(
+						getAllUsers(),
+						TE.map((userList) =>
+							userList.users.filter((user) => {
+								const index = clientDetails.users.findIndex(
+									(cUser) => cUser.id === user.id
+								);
+								return index === -1;
+							})
+						),
+						TE.map(
+							(users): LoadingContainer => ({
+								client: clientDetails,
+								allUsers: users
+							})
+						)
+					)
+				),
+				TE.map((container) => {
+					setState((draft) => {
+						draft.clientName = container.client.name;
+						draft.allRoles = container.client.roles;
+						draft.clientUsers = container.client.users;
+						draft.allUsers = container.allUsers;
+					});
 				})
-			),
-		[loadFullClientDetails, loadUsers]
+			)(),
+		[setState, state.clientId]
 	);
 
-	const removeUser = async (userId: number) => {
-		await removeUserFromClient(userId, state.clientId)();
+	const removeUser = (userId: number) =>
+		pipe(
+			removeUserFromClient(userId, state.clientId),
+			TE.map(() => {
+				pipe(
+					state.selectedUser,
+					oMap((selectedUser: ClientUser) => {
+						if (selectedUser.id === userId) {
+							setState((draft) => {
+								draft.selectedUser = none;
+							});
+						}
+					})
+				);
+				loadEverything();
+			})
+		)();
+
+	const getSelectedUserId = (): number =>
 		pipe(
 			state.selectedUser,
-			oMap((selectedUser: ClientUser) => {
-				if (selectedUser.id === userId) {
-					setState((draft) => {
-						draft.selectedUser = none;
-					});
-				}
-			})
+			oMap((selectedUser) => selectedUser.id),
+			oGetOrElse(() => 0)
 		);
-		await loadAll();
+
+	const saveAddRole = (roleId: number) => {
+		const selectedUserId = getSelectedUserId();
+
+		pipe(
+			addRoleToUser(selectedUserId, state.clientId, roleId),
+			TE.chain(() => TE.tryCatch(loadEverything, (error) => error)),
+			TE.map(() => {
+				setState((draft) => {
+					draft.selectedUser = fromNullable(
+						draft.clientUsers.find(
+							(user) => user.id === selectedUserId
+						)
+					);
+				});
+			})
+		)();
+	};
+
+	const removeRole = (roleId: number) => {
+		const selectedUserId = getSelectedUserId();
+
+		pipe(
+			removeRoleFromUser(selectedUserId, state.clientId, roleId),
+			TE.chain(() => TE.tryCatch(loadEverything, (error) => error)),
+			TE.map(() => {
+				setState((draft) => {
+					draft.selectedUser = fromNullable(
+						draft.clientUsers.find(
+							(user) => user.id === selectedUserId
+						)
+					);
+				});
+			})
+		)();
 	};
 
 	useEffect(() => {
-		loadAll();
-	}, [loadAll]);
+		loadEverything();
+	}, [loadEverything]);
 
-	const saveAddRole = (roleId: number) =>
+	const saveAddUser = (userId: number) =>
 		pipe(
-			state.selectedUser,
-			oMap(async (selectedUser) => {
-				await addRoleToUser(selectedUser.id, state.clientId, roleId)();
-				await loadAll();
-				setState((draft) => {
-					pipe(
-						draft.selectedUser,
-						oMap((oldSelectedUser) => {
-							draft.selectedUser = fromNullable(
-								draft.clientUsers.find(
-									(user) => user.id === oldSelectedUser.id
-								)
-							);
-						})
-					);
-				});
+			addUserToClient(userId, state.clientId),
+			TE.map(() => {
+				loadEverything();
 			})
-		);
-
-	const removeRole = (roleId: number) =>
-		pipe(
-			state.selectedUser,
-			oMap(async (selectedUser) => {
-				await removeRoleFromUser(
-					selectedUser.id,
-					state.clientId,
-					roleId
-				)();
-				await loadAll();
-				setState((draft) => {
-					pipe(
-						draft.selectedUser,
-						oMap((oldSelectedUser) => {
-							draft.selectedUser = fromNullable(
-								draft.clientUsers.find(
-									(user) => user.id === oldSelectedUser.id
-								)
-							);
-						})
-					);
-				});
-			})
-		);
-
-	const saveAddUser = async (userId: number) => {
-		await addUserToClient(userId, state.clientId)();
-		await loadAll();
-	};
+		)();
 
 	const selectUser = (user: ClientUser) =>
 		setState((draft) => {
---
+++ test/components/Root/Content/Clients/ClientDetails/ClientGrants.test.tsx
@@ -0,0 +1,612 @@
+import { createTestReduxProvider } from '@craigmiller160/react-test-utils';
+import { createMemoryHistory, MemoryHistory } from 'history';
+import { render, screen, waitFor } from '@testing-library/react';
+import { Route, Router, Switch } from 'react-router';
+import React from 'react';
+import ClientGrants from '../../../../../../src/components/Root/Content/Clients/ClientDetails/ClientGrants';
+import {
+	ClientUser,
+	FullClientDetails
+} from '../../../../../../src/types/client';
+import {
+	addUserToClient,
+	getFullClientDetails,
+	removeUserFromClient
+} from '../../../../../../src/services/ClientService';
+import * as TE from 'fp-ts/es6/TaskEither';
+import {
+	addRoleToUser,
+	getAllUsers,
+	removeRoleFromUser
+} from '../../../../../../src/services/UserService';
+import { UserDetails, UserList } from '../../../../../../src/types/user';
+import userEvent from '@testing-library/user-event';
+import { Role } from '../../../../../../src/types/role';
+
+jest.mock('../../../../../../src/services/ClientService', () => ({
+	getFullClientDetails: jest.fn(),
+	addUserToClient: jest.fn(),
+	removeUserFromClient: jest.fn()
+}));
+
+jest.mock('../../../../../../src/services/UserService', () => ({
+	getAllUsers: jest.fn(),
+	addRoleToUser: jest.fn(),
+	removeRoleFromUser: jest.fn()
+}));
+
+const [TestReduxProvider] = createTestReduxProvider({});
+
+const doRender = (history: MemoryHistory) =>
+	waitFor(() =>
+		render(
+			<TestReduxProvider>
+				<Router history={history}>
+					<Switch>
+						<Route
+							path="/clients/:id/grants"
+							component={ClientGrants}
+						/>
+					</Switch>
+				</Router>
+			</TestReduxProvider>
+		)
+	);
+
+const role1: Role = {
+	id: 1,
+	name: 'MyRole',
+	clientId: 1
+};
+const role2: Role = {
+	id: 2,
+	name: 'MyRole2',
+	clientId: 1
+};
+
+const user1: UserDetails = {
+	id: 1,
+	email: 'user1@gmail.com',
+	firstName: 'user1-first',
+	lastName: 'user1-last',
+	enabled: true
+};
+
+const clientUser1: ClientUser = {
+	...user1,
+	roles: []
+};
+
+const user2: UserDetails = {
+	id: 2,
+	email: 'user2@gmail.com',
+	firstName: 'user2-first',
+	lastName: 'user2-last',
+	enabled: true
+};
+
+const clientUser2: ClientUser = {
+	...user2,
+	roles: []
+};
+
+const client: FullClientDetails = {
+	id: 1,
+	name: 'Client Name',
+	clientKey: 'Key',
+	enabled: true,
+	accessTokenTimeoutSecs: 100,
+	refreshTokenTimeoutSecs: 200,
+	authCodeTimeoutSecs: 300,
+	redirectUris: ['https://www.google.com', 'https://www.facebook.com'],
+	roles: [],
+	users: [clientUser1]
+};
+
+const userList: UserList = {
+	users: [user1, user2]
+};
+
+const mockGetFullClientDetails = (clientArg: FullClientDetails) =>
+	(getFullClientDetails as jest.Mock).mockImplementationOnce(() =>
+		TE.right(clientArg)
+	);
+
+const mockGetAllUsers = (userListArg: UserList) =>
+	(getAllUsers as jest.Mock).mockImplementationOnce(() =>
+		TE.right(userListArg)
+	);
+
+const mockAddUserToClient = () =>
+	(addUserToClient as jest.Mock).mockImplementationOnce(() => TE.right([]));
+
+const mockRemoveUserFromClient = () =>
+	(removeUserFromClient as jest.Mock).mockImplementation(() => TE.right([]));
+
+const mockAddRoleToUser = () =>
+	(addRoleToUser as jest.Mock).mockImplementation(() => TE.right([]));
+
+const mockRemoveRoleFromUser = () =>
+	(removeRoleFromUser as jest.Mock).mockImplementation(() => TE.right([]));
+
+const getAddUserBtn = () =>
+	screen.getByRole('button', {
+		name: 'Add User'
+	});
+
+describe('ClientGrants', () => {
+	let testHistory: MemoryHistory;
+	beforeEach(() => {
+		jest.resetAllMocks();
+		testHistory = createMemoryHistory();
+		testHistory.push('/clients/1/grants');
+
+		mockAddUserToClient();
+		mockRemoveUserFromClient();
+		mockAddRoleToUser();
+		mockRemoveRoleFromUser();
+	});
+
+	describe('rendering', () => {
+		it('renders with users', async () => {
+			mockGetFullClientDetails(client);
+			mockGetAllUsers(userList);
+
+			await doRender(testHistory);
+
+			expect(screen.queryByText('Client Name')).toBeInTheDocument();
+			expect(screen.queryByText('Users')).toBeInTheDocument();
+			expect(
+				screen.queryByText(`${user1.firstName} ${user1.lastName}`)
+			).toBeInTheDocument();
+			expect(screen.queryByText('Roles')).toBeInTheDocument();
+			const addUserButton = getAddUserBtn();
+			expect(addUserButton).not.toBeDisabled();
+
+			expect(getFullClientDetails).toHaveBeenCalledWith(1);
+			expect(getAllUsers).toHaveBeenCalled();
+		});
+
+		it('renders without users', async () => {
+			mockGetFullClientDetails({
+				...client,
+				users: []
+			});
+			mockGetAllUsers(userList);
+
+			await doRender(testHistory);
+
+			expect(screen.queryByText('Client Name')).toBeInTheDocument();
+			expect(screen.queryByText('Users')).toBeInTheDocument();
+			expect(screen.queryByText('No Users')).toBeInTheDocument();
+			expect(screen.queryByText('Roles')).toBeInTheDocument();
+			const addUserButton = getAddUserBtn();
+			expect(addUserButton).not.toBeDisabled();
+
+			expect(getFullClientDetails).toHaveBeenCalledWith(1);
+			expect(getAllUsers).toHaveBeenCalled();
+		});
+
+		it('renders without users with add button disabled', async () => {
+			mockGetFullClientDetails({
+				...client,
+				users: []
+			});
+			mockGetAllUsers({
+				users: []
+			});
+
+			await doRender(testHistory);
+
+			expect(screen.queryByText('Client Name')).toBeInTheDocument();
+			expect(screen.queryByText('Users')).toBeInTheDocument();
+			expect(screen.queryByText('No Users')).toBeInTheDocument();
+			expect(screen.queryByText('Roles')).toBeInTheDocument();
+			const addUserButton = getAddUserBtn();
+			expect(addUserButton).toBeDisabled();
+
+			expect(getFullClientDetails).toHaveBeenCalledWith(1);
+			expect(getAllUsers).toHaveBeenCalled();
+		});
+	});
+
+	describe('behavior', () => {
+		it('add a user, with no more users available to add', async () => {
+			mockGetFullClientDetails(client);
+			mockGetFullClientDetails({
+				...client,
+				users: [clientUser1, clientUser2]
+			});
+			mockGetAllUsers(userList);
+			mockGetAllUsers(userList);
+
+			await doRender(testHistory);
+
+			expect(
+				screen.queryByText(`${user2.firstName} ${user2.lastName}`)
+			).not.toBeInTheDocument();
+
+			const addUserButton = getAddUserBtn();
+			expect(addUserButton).not.toBeDisabled();
+
+			await waitFor(() => userEvent.click(addUserButton));
+
+			expect(screen.queryByLabelText('User')).toBeInTheDocument();
+			expect(screen.queryByText('Select')).toBeInTheDocument();
+			expect(screen.queryByText('Cancel')).toBeInTheDocument();
+
+			userEvent.click(screen.getByLabelText('User'));
+			userEvent.click(screen.getByText('user2@gmail.com'));
+			expect(screen.getByLabelText('User')).toHaveValue(
+				'user2@gmail.com'
+			);
+
+			await waitFor(() =>
+				userEvent.click(
+					screen.getByRole('button', {
+						name: 'Select'
+					})
+				)
+			);
+
+			expect(
+				screen.queryByText(`${user2.firstName} ${user2.lastName}`)
+			).toBeInTheDocument();
+
+			expect(addUserButton).toBeDisabled();
+
+			expect(addUserToClient).toHaveBeenCalledWith(2, 1);
+		});
+
+		it('go to a user', async () => {
+			mockGetFullClientDetails(client);
+			mockGetAllUsers(userList);
+
+			await doRender(testHistory);
+
+			await waitFor(() =>
+				userEvent.click(
+					screen.getByRole('button', {
+						name: 'Go'
+					})
+				)
+			);
+
+			expect(testHistory.location.pathname).toEqual('/users/1');
+		});
+
+		it('remove a user', async () => {
+			mockGetFullClientDetails(client);
+			mockGetFullClientDetails({
+				...client,
+				users: []
+			});
+			mockGetAllUsers(userList);
+			mockGetAllUsers(userList);
+
+			await doRender(testHistory);
+
+			expect(
+				screen.queryByText(`${user1.firstName} ${user1.lastName}`)
+			).toBeInTheDocument();
+
+			await waitFor(() =>
+				userEvent.click(
+					screen.getByRole('button', {
+						name: 'Remove'
+					})
+				)
+			);
+
+			expect(
+				screen.queryByText('Are you sure you want to remove this user?')
+			).toBeInTheDocument();
+			expect(screen.queryByText('Confirm')).toBeInTheDocument();
+			expect(screen.queryByText('Cancel')).toBeInTheDocument();
+
+			await waitFor(() =>
+				userEvent.click(
+					screen.getByRole('button', {
+						name: 'Confirm'
+					})
+				)
+			);
+
+			await waitFor(() =>
+				expect(screen.queryByText('Confirm')).not.toBeInTheDocument()
+			);
+
+			expect(
+				screen.queryByText(`${user1.firstName} ${user1.lastName}`)
+			).not.toBeInTheDocument();
+		});
+
+		it('selects a user but no roles are available', async () => {
+			mockGetFullClientDetails(client);
+			mockGetAllUsers(userList);
+
+			await doRender(testHistory);
+
+			expect(screen.queryByText('No Roles')).not.toBeInTheDocument();
+			expect(screen.queryByText('Add Role')).not.toBeInTheDocument();
+
+			const userListItem = screen.getByText(
+				`${user1.firstName} ${user1.lastName}`
+			);
+			userEvent.click(userListItem);
+
+			expect(screen.queryByText('No Roles')).toBeInTheDocument();
+			expect(
+				screen.queryByRole('button', {
+					name: 'Add Role'
+				})
+			).toBeDisabled();
+		});
+
+		it('selects a user and adds a role', async () => {
+			mockGetFullClientDetails({
+				...client,
+				roles: [role1]
+			});
+			mockGetFullClientDetails({
+				...client,
+				users: [
+					{
+						...clientUser1,
+						roles: [role1]
+					}
+				],
+				roles: [role1]
+			});
+			mockGetAllUsers(userList);
+			mockGetAllUsers(userList);
+
+			await doRender(testHistory);
+
+			expect(screen.queryByText('No Roles')).not.toBeInTheDocument();
+			expect(screen.queryByText('Add Role')).not.toBeInTheDocument();
+			expect(screen.queryByText('MyRole')).not.toBeInTheDocument();
+
+			const userListItem = screen.getByText(
+				`${user1.firstName} ${user1.lastName}`
+			);
+			userEvent.click(userListItem);
+
+			expect(screen.queryByText('No Roles')).toBeInTheDocument();
+			expect(
+				screen.queryByRole('button', {
+					name: 'Add Role'
+				})
+			).not.toBeDisabled();
+
+			await waitFor(() =>
+				userEvent.click(
+					screen.getByRole('button', {
+						name: 'Add Role'
+					})
+				)
+			);
+
+			expect(screen.queryByText('Select')).toBeInTheDocument();
+			expect(screen.queryByText('Cancel')).toBeInTheDocument();
+
+			await waitFor(() => userEvent.click(screen.getByLabelText('Role')));
+			await waitFor(() => userEvent.click(screen.getByText('MyRole')));
+
+			expect(screen.getByLabelText('Role')).toHaveValue('MyRole');
+
+			await waitFor(() =>
+				userEvent.click(
+					screen.getByRole('button', {
+						name: 'Select'
+					})
+				)
+			);
+
+			await waitFor(() =>
+				expect(screen.queryByText('Select')).not.toBeInTheDocument()
+			);
+
+			expect(screen.queryByText('No Roles')).not.toBeInTheDocument();
+			expect(screen.queryAllByText('MyRole')).toHaveLength(2);
+
+			expect(addRoleToUser).toHaveBeenCalledWith(1, 1, 1);
+		});
+
+		it('selects a user and removes a role', async () => {
+			mockGetFullClientDetails({
+				...client,
+				users: [
+					{
+						...clientUser1,
+						roles: [role1]
+					}
+				],
+				roles: [role1]
+			});
+			mockGetFullClientDetails({
+				...client,
+				roles: [role1]
+			});
+			mockGetAllUsers(userList);
+			mockGetAllUsers(userList);
+
+			await doRender(testHistory);
+
+			const userListItem = screen.getByText(
+				`${user1.firstName} ${user1.lastName}`
+			);
+			userEvent.click(userListItem);
+
+			expect(screen.queryByText('No Roles')).not.toBeInTheDocument();
+			expect(screen.queryAllByText('MyRole')).toHaveLength(2);
+
+			await waitFor(() =>
+				userEvent.click(screen.queryAllByText('Remove')[1])
+			);
+
+			expect(
+				screen.queryByText('Are you sure you want to remove this role?')
+			).toBeInTheDocument();
+			expect(screen.queryByText('Confirm')).toBeInTheDocument();
+			expect(screen.queryByText('Cancel')).toBeInTheDocument();
+
+			await waitFor(() => userEvent.click(screen.getByText('Confirm')));
+			await waitFor(() =>
+				expect(screen.queryByText('Confirm')).not.toBeInTheDocument()
+			);
+
+			expect(screen.queryByText('No Roles')).toBeInTheDocument();
+			expect(screen.queryAllByText('MyRole')).not.toHaveLength(2);
+
+			expect(removeRoleFromUser).toHaveBeenCalledWith(1, 1, 1);
+		});
+
+		it('cancel add user dialog', async () => {
+			mockGetFullClientDetails({
+				...client,
+				users: [
+					{
+						...clientUser1,
+						roles: [role1]
+					}
+				],
+				roles: [role1]
+			});
+			mockGetAllUsers(userList);
+
+			await doRender(testHistory);
+
+			expect(screen.queryByLabelText('User')).not.toBeInTheDocument();
+
+			await waitFor(() =>
+				userEvent.click(
+					screen.getByRole('button', {
+						name: 'Add User'
+					})
+				)
+			);
+			expect(screen.queryByLabelText('User')).toBeInTheDocument();
+
+			await waitFor(() =>
+				userEvent.click(
+					screen.getByRole('button', {
+						name: 'Cancel'
+					})
+				)
+			);
+
+			await waitFor(() =>
+				expect(screen.queryByLabelText('User')).not.toBeInTheDocument()
+			);
+		});
+
+		it('cancel add role dialog', async () => {
+			mockGetFullClientDetails({
+				...client,
+				users: [
+					{
+						...clientUser1,
+						roles: [role1]
+					}
+				],
+				roles: [role1, role2]
+			});
+			mockGetAllUsers(userList);
+
+			await doRender(testHistory);
+
+			const userListItem = screen.getByText(
+				`${user1.firstName} ${user1.lastName}`
+			);
+			userEvent.click(userListItem);
+
+			expect(screen.queryByLabelText('Role')).not.toBeInTheDocument();
+
+			const addRoleButton = screen.getByRole('button', {
+				name: 'Add Role'
+			});
+			expect(addRoleButton).not.toBeDisabled();
+
+			await waitFor(() => userEvent.click(screen.getByText('Add Role')));
+			expect(screen.queryByLabelText('Role')).toBeInTheDocument();
+
+			await waitFor(() => userEvent.click(screen.getByText('Cancel')));
+			await waitFor(() =>
+				expect(screen.queryByLabelText('Role')).not.toBeInTheDocument()
+			);
+		});
+
+		it('cancel remove user dialog', async () => {
+			mockGetFullClientDetails({
+				...client,
+				users: [
+					{
+						...clientUser1,
+						roles: [role1]
+					}
+				],
+				roles: [role1, role2]
+			});
+			mockGetAllUsers(userList);
+
+			await doRender(testHistory);
+
+			const removeUserMessage =
+				'Are you sure you want to remove this user?';
+
+			expect(
+				screen.queryByText(removeUserMessage)
+			).not.toBeInTheDocument();
+
+			await waitFor(() => userEvent.click(screen.getByText('Remove')));
+			expect(screen.queryByText(removeUserMessage)).toBeInTheDocument();
+
+			await waitFor(() => userEvent.click(screen.getByText('Cancel')));
+			await waitFor(() =>
+				expect(
+					screen.queryByText(removeUserMessage)
+				).not.toBeInTheDocument()
+			);
+		});
+
+		it('cancel remove role dialog', async () => {
+			mockGetFullClientDetails({
+				...client,
+				users: [
+					{
+						...clientUser1,
+						roles: [role1]
+					}
+				],
+				roles: [role1, role2]
+			});
+			mockGetAllUsers(userList);
+
+			await doRender(testHistory);
+
+			const userListItem = screen.getByText(
+				`${user1.firstName} ${user1.lastName}`
+			);
+			userEvent.click(userListItem);
+
+			const removeRoleMessage =
+				'Are you sure you want to remove this role?';
+
+			expect(
+				screen.queryByText(removeRoleMessage)
+			).not.toBeInTheDocument();
+			await waitFor(() =>
+				userEvent.click(screen.getAllByText('Remove')[1])
+			);
+			expect(screen.queryByText(removeRoleMessage)).toBeInTheDocument();
+
+			await waitFor(() => userEvent.click(screen.getByText('Cancel')));
+			await waitFor(() =>
+				expect(
+					screen.queryByText(removeRoleMessage)
+				).not.toBeInTheDocument()
+			);
+		});
+	});
+});
