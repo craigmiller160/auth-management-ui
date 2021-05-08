64f8b8a890fcb7d2742b6ad4a78a281acb255296
Craig Miller <craigmiller160@gmail.com>
2021-05-08 10:23:07 -0400

    Fixed lint errors

--- test/components/Root/Content/Users/UserDetails/UserGrants.test.tsx
+++ test/components/Root/Content/Users/UserDetails/UserGrants.test.tsx
@@ -1,55 +1,55 @@
 import React from 'react';
 
 describe('UserGrants', () => {
-    describe('rendering', () => {
-        it('renders with clients', () => {
-            throw new Error();
-        });
+	describe('rendering', () => {
+		it('renders with clients', () => {
+			throw new Error();
+		});
 
-        it('renders without clients', () => {
-            throw new Error();
-        });
+		it('renders without clients', () => {
+			throw new Error();
+		});
 
-        it('renders without clients with add button disabled', () => {
-            throw new Error();
-        });
-    });
+		it('renders without clients with add button disabled', () => {
+			throw new Error();
+		});
+	});
 
-    describe('behavior', () => {
-        it('add a client, with no more clients available to add', () => {
-            throw new Error();
-        });
+	describe('behavior', () => {
+		it('add a client, with no more clients available to add', () => {
+			throw new Error();
+		});
 
-        it('go to a client', () => {
-            throw new Error();
-        });
+		it('go to a client', () => {
+			throw new Error();
+		});
 
-        it('select a client, but no roles available', () => {
-            throw new Error();
-        });
+		it('select a client, but no roles available', () => {
+			throw new Error();
+		});
 
-        it('select a client and add a role', () => {
-            throw new Error();
-        });
+		it('select a client and add a role', () => {
+			throw new Error();
+		});
 
-        it('select a client and remove a role', () => {
-            throw new Error();
-        });
+		it('select a client and remove a role', () => {
+			throw new Error();
+		});
 
-        it('cancel add client dialog', () => {
-            throw new Error();
-        });
+		it('cancel add client dialog', () => {
+			throw new Error();
+		});
 
-        it('cancel add role dialog', () => {
-            throw new Error();
-        });
+		it('cancel add role dialog', () => {
+			throw new Error();
+		});
 
-        it('cancel remove client dialog', () => {
-            throw new Error();
-        });
+		it('cancel remove client dialog', () => {
+			throw new Error();
+		});
 
-        it('cancel remove role dialog', () => {
-            throw new Error();
-        });
-    });
+		it('cancel remove role dialog', () => {
+			throw new Error();
+		});
+	});
 });
