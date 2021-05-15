e193fb2cd527ac8215db58512c85c95683eb9f1d
Craig <craigmiller160@gmail.com>
2021-05-15 15:05:59 -0400

    Migrated to ajax error handler library

--- src/services/AjaxApi.ts
+++ src/services/AjaxApi.ts
@@ -1,5 +1,32 @@
 import { createApi } from '@craigmiller160/ajax-api-fp-ts';
-import ajaxErrorHandler from './ajaxErrorHandler';
+import createAjaxErrorHandler from '@craigmiller160/ajax-error-handler';
+import { AxiosResponse } from 'axios';
+import { ErrorResponse } from '../types/api';
+import store from '../store';
+import { showErrorReduxAlert } from '@craigmiller160/react-material-ui-common';
+import authSlice from '../store/auth/slice';
+import { none } from 'fp-ts/es6/Option';
+
+interface MaybeErrorResponse {
+	status?: number;
+	message?: string;
+}
+
+const isErrorResponse = (data?: MaybeErrorResponse): data is ErrorResponse =>
+	data?.status !== undefined && data?.message !== undefined;
+
+const ajaxErrorHandler = createAjaxErrorHandler({
+	responseMessageExtractor: (response: AxiosResponse) => {
+		if (isErrorResponse(response.data)) {
+			return response.data.message;
+		}
+		return '';
+	},
+	errorMessageHandler: (message: string) =>
+		store.dispatch(showErrorReduxAlert(message)),
+	unauthorizedHandler: () =>
+		store.dispatch(authSlice.actions.setUserData(none))
+});
 
 export default createApi({
 	baseURL: '/auth-management/api',
