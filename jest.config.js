3fd69cb6c57c8b1176502c7d0a3a36eb3aaf3819
Craig <craigmiller160@gmail.com>
2021-12-29 18:08:55 -0500

    Added new jest stuff

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
-const jestConfig = require('@craigmiller160/react-web-config/jest');
+const jestConfig = require('@craigmiller160/jest-config');
+const jestConfigBabel = require('@craigmiller160/jest-config-babel');
 
 module.exports = {
-	...jestConfig
+	...jestConfig,
+	...jestConfigBabel
 };
