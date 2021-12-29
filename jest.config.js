4c99fcaf7197d777f99878ec916508d686a86e31
Craig <craigmiller160@gmail.com>
2021-12-29 18:14:28 -0500

    Added more jest config libs

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 const jestConfig = require('@craigmiller160/jest-config');
 const jestConfigBabel = require('@craigmiller160/jest-config-babel');
+const jestConfigTs = require('@craigmiller160/jest-config-ts');
 
 module.exports = {
 	...jestConfig,
-	...jestConfigBabel
+	...jestConfigBabel,
+	...jestConfigTs
 };
