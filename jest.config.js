5c4c8d11517d4866dbcf22f43a7f1da0c88f684c
Craig <craigmiller160@gmail.com>
2021-12-29 18:29:18 -0500

    Added TODO

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 const jestConfig = require('@craigmiller160/jest-config');
 const jestConfigBabel = require('@craigmiller160/jest-config-babel');
 const jestConfigTs = require('@craigmiller160/jest-config-ts');
-const { merge } = require('lodash');
+const { merge } = require('lodash'); // TODO maybe i can only add this one function?
 
 const result = merge(merge(jestConfig, jestConfigBabel), jestConfigTs);
 console.log(result);
