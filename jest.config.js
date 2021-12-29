c71d6aa1101f084a5f5a704e8e39320c2d62c37b
Craig <craigmiller160@gmail.com>
2021-12-29 18:28:38 -0500

    Making some progress

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 const jestConfig = require('@craigmiller160/jest-config');
 const jestConfigBabel = require('@craigmiller160/jest-config-babel');
 const jestConfigTs = require('@craigmiller160/jest-config-ts');
+const { merge } = require('lodash');
 
-module.exports = {
-	...jestConfig,
-	...jestConfigBabel,
-	...jestConfigTs
-};
+const result = merge(merge(jestConfig, jestConfigBabel), jestConfigTs);
+console.log(result);
+
+module.exports = result;
