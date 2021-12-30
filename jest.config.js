5e7a4e309c853806cd4c9bd755d7e5a1eca636b8
Craig <craigmiller160@gmail.com>
2021-12-29 19:19:41 -0500

    Making a LOT of progress now

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,4 @@ const jestConfigBabel = require('@craigmiller160/jest-config-babel');
 const jestConfigTs = require('@craigmiller160/jest-config-ts');
 const { merge } = require('lodash'); // TODO maybe i can only add this one function?
 
-const result = merge(merge(jestConfig, jestConfigBabel), jestConfigTs);
-console.log(result);
-
-module.exports = result;
+module.exports = merge(merge(jestConfig, jestConfigBabel), jestConfigTs);
