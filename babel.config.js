5b6443f8dac15a3aa3659d4ce61f96ed97b2bc6a
Craig <craigmiller160@gmail.com>
2021-12-30 16:05:00 -0500

    Updated all libraries

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,5 @@
+const configMerge = require('@craigmiller160/config-merge');
 const babelConfig = require('@craigmiller160/babel-config');
 const babelConfigReact = require('@craigmiller160/babel-config-react');
-const { merge } = require('lodash'); // TODO try and just use single function
 
-const config = merge(babelConfig, babelConfigReact);
-
-module.exports = config;
+module.exports = configMerge(babelConfig, babelConfigReact);
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
+const configMerge = require('@craigmiller160/config-merge');
 const jestConfig = require('@craigmiller160/jest-config');
-const jestConfigBabel = require('@craigmiller160/jest-config-babel');
 const jestConfigTs = require('@craigmiller160/jest-config-ts');
-const { merge } = require('lodash'); // TODO maybe i can only add this one function?
 
-module.exports = merge(merge(jestConfig, jestConfigBabel), jestConfigTs);
+module.exports = configMerge(jestConfig, jestConfigTs);
