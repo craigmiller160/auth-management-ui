6a1c6b7e4fa32d6d45290e24fa7c143a6c64c930
Craig <craigmiller160@gmail.com>
2021-12-30 19:31:15 -0500

    Finished integrating new webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,37 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const config = require('@craigmiller160/webpack-config');
 
-console.log(config);
+const localDevServerConfig = {
+    devServer: {
+        port: 3000,
+        https: true,
+        proxy: {
+            '/auth-management/api': {
+                target: 'https://localhost:7004',
+                changeOrigin: true,
+                secure: false,
+                pathRewrite: {
+                    '^/auth-management/api': ''
+                },
+                logLevel: 'debug'
+            },
+            '/auth-management/oauth2': {
+                target: 'https://localhost:7003',
+                changeOrigin: true,
+                secure: false,
+                pathRewrite: {
+                    '^/auth-management/oauth2': ''
+                },
+                logLevel: 'debug'
+            }
+        }
+    }
+};
+
+const parts = [config];
+
+if (process.env.NODE_ENV === 'development') {
+    parts.push(localDevServerConfig);
+}
+
+module.exports = merge(parts);
